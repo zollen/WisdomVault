d573ac69c5910fc79611deb2efc9968eb64aa196
Stephen Kong <zollen.kong@gmail.com>
2018-09-10 18:23:02 -0400

    fix w of DFT matrix

--- src/FourierMatrixDefinition.java
+++ src/FourierMatrixDefinition.java
@@ -5,7 +5,7 @@ public class FourierMatrixDefinition {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Equation eq = new Equation();
-		// w = e^(i * k * 2pi/n)
+		// w = e^(i * k * 2pi/n) * -1
 		// w = [ 1,   1,    1,    1,    1,    1 ]
 		//	   [ 1,   w,  w^2,  w^3,  w^4,  w^5 ]
 		// 	   [ 1, w^2,  w^4,  w^6,  w^8, w^10 ]
