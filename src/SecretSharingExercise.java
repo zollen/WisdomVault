5c829d309bbc119f74607f188db69c72651158e1
Stephen Kong <zollen.kong@gmail.com>
2018-09-20 19:55:07 -0400

    classification exercise

--- src/SecretSharingExercise.java
+++ src/SecretSharingExercise.java
@@ -10,16 +10,16 @@ public class SecretSharingExercise {
 			// single byte encryption & decryption
 			
 			byte input = 0b00110100;
-			byte key1 = 0b01011011;
-			byte key2 = 0b00010111;
+			byte salt1 = 0b01011011;
+			byte salt2 = 0b00010111;
 
 			System.out.println("RESULT: " + (int) input);
-			System.out.println("RESULT: " + (int) key1);
-			System.out.println("RESULT: " + (int) key2);
+			System.out.println("RESULT: " + (int) salt1);
+			System.out.println("RESULT: " + (int) salt2);
 
-			System.out.println("RESULT: " + (int) (input ^ key1 ^ key2));
+			System.out.println("RESULT: " + (int) (input ^ salt1 ^ salt2));
 
-			System.out.println("RESULT: " + (int) (input ^ key1 ^ key2 ^ key1 ^ key2));
+			System.out.println("RESULT: " + (int) (input ^ salt1 ^ salt2 ^ salt1 ^ salt2));
 		}
 
 		{
@@ -27,19 +27,19 @@ public class SecretSharingExercise {
 
 			String message = "This is a great learning experience!!!";
 			byte [] msg = message.getBytes();
-			byte [] key1 = new byte[msg.length];
-			byte [] key2 = new byte[msg.length];
-			byte [] key3 = new byte[msg.length];
+			byte [] salt1 = new byte[msg.length];
+			byte [] salt2 = new byte[msg.length];
+			byte [] salt3 = new byte[msg.length];
 			byte[] encrypted = new byte[msg.length];
 
 			Random rand = new Random(System.nanoTime());
-			rand.nextBytes(key1);
-			rand.nextBytes(key2);
-			rand.nextBytes(key3);
+			rand.nextBytes(salt1);
+			rand.nextBytes(salt2);
+			rand.nextBytes(salt3);
 
 			// encryption
 			for (int i = 0; i < msg.length; i++) {
-				encrypted[i] = (byte) (msg[i] ^ key1[i] ^ key2[i] ^ key3[i]);
+				encrypted[i] = (byte) (msg[i] ^ salt1[i] ^ salt2[i] ^ salt3[i]);
 			}
 
 			System.out.println("Encrypted: " + new String(Base64.getEncoder().encode(encrypted)));
@@ -47,7 +47,7 @@ public class SecretSharingExercise {
 			// decryption
 			byte[] decrypted = new byte[msg.length];
 			for (int i = 0; i < msg.length; i++) {
-				decrypted[i] = (byte) (encrypted[i] ^ key2[i] ^ key1[i] ^ key3[i]);
+				decrypted[i] = (byte) (encrypted[i] ^ salt2[i] ^ salt1[i] ^ salt3[i]);
 			}
 
 			System.out.println("Decrypted: " + new String(decrypted));
---
+++ src/TextRecogExercise.java
@@ -0,0 +1,82 @@
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.equation.Equation;
+
+public class TextRecogExercise {
+	
+	
+	/*
+	 * Attributes
+	 * ----------
+	 * friend => personal
+	 * hello => personal
+	 * reward => spam
+	 * money => spam
+	 * 
+	 * Message                  |  friend   | hello   | reward   | money    | Conclusion
+	 * ---------------------------------------------------------------------------------
+	 * I am good                |  0        | 1       | 0        | 0        | P
+	 * Collect my money         |  0        | 0       | 0        | 1        | S
+	 * Hello? How are you       |  0        | 1       | 0        | 0        | P
+	 * Reward will be big       |  0        | 0       | 1        | 0        | S  
+	 * My friend is coming      |  1        | 0       | 0        | 0        | P
+	 * friend needs money       |  1        | 0       | 0        | 1        | P
+ 	 * The reward will be money |  0        | 0       | 1        | 1        | S
+ 	 * Hello, I need some money |  0        | 1       | 0        | 1        | P
+ 	 * Hello friend!            |  1        | 1       | 0        | 0        | P
+ 	 * I lost my money          |  0        | 0       | 0        | 1        | S
+	 * Hello! you want money?   |  0        | 1       | 0        | 1        | P
+	 * granting great reward!   |  0        | 0       | 1        | 0        | S
+	 * hello! reward is here    |  0        | 1       | 1        | 0        | P
+	 * hello friend!!           |  1        | 1       | 0        | 0        | P
+	 * 
+	 */
+	
+	public static void main(String [] args) {
+		
+		Equation eq = new Equation();
+		
+		eq.process("A = [ " +
+						" 0, 1, 0, 0, 1;" +
+						" 0, 0, 0, 1, 0;" +
+						" 0, 1, 0, 0, 1;" +
+						" 0, 0, 1, 0, 0;" +
+						" 1, 0, 0, 0, 1;" +
+						" 1, 0, 0, 1, 1;" +
+						" 0, 0, 1, 1, 0;" +
+						" 0, 1, 0, 1, 1;" +
+						" 1, 1, 0, 0, 1;" +
+						" 0, 0, 0, 1, 0;" +
+						" 0, 1, 0, 1, 1;" +
+						" 0, 0, 1, 0, 0;" +
+						" 0, 1, 1, 0, 1;" +
+						" 1, 1, 0, 0, 1 " +
+					"]");
+		
+		DMatrixRMaj A = eq.lookupDDRM("A");
+		
+		// Freq(Personal) = #T(Personal) / #T                         = 9 / 14
+		// Freq(Personal, friend) = #T(Personal, friend) / #T(friend) = 4 / 4
+		// Freq(Personal, hello) = #T(Personal, hello) / #T(hello)    = 7 / 7		
+		// Freq(Personal, reward) = #T(Personal, reward) / #T(reward) = 1 / 4 		
+		// Freq(Personal, money) = #T(Personal, money) / #T(money)    = 3 / 6
+	
+		// Freq(Spam) = #T(Spam) / #T                                 = 5 / 14		
+		// Freq(Spam, friend) = #T(Spam, friend) / #T(friend)         = 0				
+		// Freq(Spam, hello) = #T(Spam, hello) / #T(hello)            = 0 	
+		// Freq(Spam, reward) = #T(Spam, reward) / #T(reward)         = 3 / 4 		
+		// Freq(Spam, money) = #T(Spam, money) / #T(money)            = 3 / 6
+
+		
+		
+		// "hello world!"          = [ 0, 1, 0, 0, ? ] 
+		// Personal: 9 / 14 * 7 / 7 = 9 / 14
+		// Spam:     5 / 14 * 0     = 0
+		// Conclusion [ 0, 1, 0, 0 ] is personal
+		
+		// money is my friend!!    = [ 1, 0, 0, 1, ? ]
+		// Personal: 9 / 14 * 4 / 4 * 3 / 6 = 9 / 28
+		// Spam:     5 / 14 * 0     * 3 / 6 = 0
+		// Conclusion [ 1, 0, 0, 1 ] is personal
+	}
+
+}
