49914f5dd9a824d538262f87e4e1e9a51c28fb50
Stephen Kong <zollen.kong@gmail.com>
2018-10-20 14:07:40 -0400

    support graph and fix zipf

---
+++ src/ZipfDemo1.java
@@ -0,0 +1,140 @@
+
+import java.awt.Color;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.LogarithmicAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+
+/**
+ * A demo showing the use of log axes.
+ *
+ */
+public class ZipfDemo1 extends ApplicationFrame {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private static final double EULER_MASCHERONI_CONSTANT = 0.5772156649d;
+	
+	private static final DecimalFormat formatter = new DecimalFormat("0.0000");
+
+	/**
+     * Creates a new demo.
+     *
+     * @param title  the frame title.
+     */
+    public ZipfDemo1(final String title, long wordsCount, List<ZipfExercise1.Word> words) {
+
+        super(title);
+
+        final XYSeries data1 = new XYSeries("USA.txt Words Analysis");
+        final XYSeries data2 = new XYSeries("Zipf Distribution Prediction");
+       
+       
+        double total = 0d;
+        for (int i = 1; i < words.size(); i++) {
+        	total += (double) 1 / Math.pow(i, EULER_MASCHERONI_CONSTANT);
+        }
+   
+        System.out.println("Rank            Word         Frequency      Probability          K             Average(K)          Deviation(K)");
+        System.out.println("===============================================================================================================");
+        List<Double> allKs = new ArrayList<Double>();
+        for (int i = 0; i < words.size(); i++) {
+        	
+        	ZipfExercise1.Word word = words.get(i);
+        	
+        	data1.add(i + 1, word.getCount());
+        	data2.add(i + 1, 10000 * (double)(1 / Math.pow((i + 1), EULER_MASCHERONI_CONSTANT) / total));
+        	
+        	double prob = (double) word.getCount() / wordsCount;
+        	double k = (double) prob * (i + 1);
+        	allKs.add(k);
+        	double avg = (double) allKs.stream().collect(Collectors.summingDouble(p -> p)) / allKs.size();
+        	
+        	System.out.println(String.format("%2d [%15s]         %4d           %5s               %5s        %5s             %7s", 
+        			(i + 1), 
+        			(word.getWord().substring(0, word.getWord().length() < 15 ? word.getWord().length() : 15)),
+        			word.getCount(),
+        			formatter.format(prob),
+        			formatter.format(k),
+        			formatter.format(avg),
+        			formatter.format(k - avg)
+        			));
+        }
+
+        final XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(data1);
+        dataset.addSeries(data2);
+       
+     
+
+        final JFreeChart chart = ChartFactory.createXYLineChart(
+            "Zipf Distribution",      		// chart title
+            "Rank",              		 	// domain axis label
+            "Frequency",                  	// range axis label
+            dataset,                  		// data
+            PlotOrientation.VERTICAL,
+            true,                     		// include legend
+            true,
+            false
+        );
+
+        final XYPlot plot = chart.getXYPlot();
+   //     final NumberAxis domainAxis = new NumberAxis("Rank");
+        final NumberAxis domainAxis = new LogarithmicAxis("Rank");
+        final NumberAxis rangeAxis = new LogarithmicAxis("Frequency");
+    
+        plot.setDomainAxis(domainAxis);
+        plot.setRangeAxis(rangeAxis);
+        chart.setBackgroundPaint(Color.white);
+        plot.setOutlinePaint(Color.black);
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(1200, 800));
+        setContentPane(chartPanel);
+
+    }
+
+    // ****************************************************************************
+    // * JFREECHART DEVELOPER GUIDE                                               *
+    // * The JFreeChart Developer Guide, written by David Gilbert, is available   *
+    // * to purchase from Object Refinery Limited:                                *
+    // *                                                                          *
+    // * http://www.object-refinery.com/jfreechart/guide.html                     *
+    // *                                                                          *
+    // * Sales are used to provide funding for the JFreeChart project - please    * 
+    // * support us so that we can continue developing free software.             *
+    // ****************************************************************************
+    
+    /**
+     * Starting point for the demonstration application.
+     *
+     * @param args  ignored.
+     */
+    public static void main(final String[] args) {
+
+    	ZipfExercise1 zipf = new ZipfExercise1("data/USA.txt");
+    	
+    	
+        final ZipfDemo1 demo = new ZipfDemo1("Zipf Distribution", zipf.getTotal(), zipf.getWords());
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+
+    }
+
+}
--- src/ZipfExercise1.java
+++ src/ZipfExercise1.java
@@ -5,36 +5,72 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class ZipfExercise1 {
+	
+	private String file = null;
+	
+	private String content = null;
+	
+	private List<Word> words = new ArrayList<Word>();
+	
+	private long total = 0;
+	
+	public ZipfExercise1(String file) {
+		this.file = file;
+		
+		this.analysis();
+	}
 
-	public static void main(String[] args) {
+	private void analysis() {
 		// TODO Auto-generated method stub
+		
 		try {
-			String buf = readFile("data/USA.txt");
+			readFile();
 			
-			String [] tokens = buf.split("[\\p{Space}\\p{Punct}\\p{Digit}]+");
+			String [] tokens = content.split("[^\\p{Alpha}]+");
+			
+			total = tokens.length;
 			
 			Map<String, Long> map = Arrays.stream(tokens).collect(Collectors.groupingBy(p -> p.toLowerCase(), Collectors.counting()));
+			
+			Map<?, ?> map2 = map.entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey, (a, b) -> a + ", " + b, TreeMap::new));
 						
-			List<Word> words = new ArrayList<Word>();
-			map.entrySet().stream().forEach(p -> {
-				words.add(new Word(p.getKey(), p.getValue()));
+			
+			map2.entrySet().stream().forEach(p -> {
+				words.add(new Word((String) p.getValue(), (Long) p.getKey()));
 			});
 			
 			Collections.sort(words);
 			
-			words.stream().forEach(p -> { System.out.println("[" + p.word + "] ===> " + p.count); });
-			
+		//	words.stream().forEach(p -> System.out.println(p.getWord() + " : " + p.getCount()) );
+		//	System.out.println("SIZE: " + words.size());
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static String readFile(String file) throws Exception {		
-		return new String(Files.readAllBytes(Paths.get(file)));
+	private void readFile() throws Exception {		
+		content = new String(Files.readAllBytes(Paths.get(file)));
+	}
+	
+	public List<Word> getWords() {
+		return words;
+	}
+
+	public void setWords(List<Word> words) {
+		this.words = words;
+	}
+
+	public long getTotal() {
+		return total;
+	}
+
+	public void setTotal(long total) {
+		this.total = total;
 	}
 	
 	public static class Word implements Comparable<Word>{
@@ -62,16 +98,16 @@ public class ZipfExercise1 {
 		public void setCount(Long count) {
 			this.count = count;
 		}
-		
-		@Override
-		public String toString() {
-			return "Word [word=" + word + ", count=" + count + "]";
-		}
 
 		@Override
 		public int compareTo(Word o) {
 			// TODO Auto-generated method stub
 			return this.count.compareTo(o.count) * -1;
+		}
+		
+		@Override
+		public String toString() {
+			return "Word [word=" + word + ", count=" + count + "]";
 		}
 	}
 
