aa313e08f2541266f2d1615db73317220abbc2ff
Stephen Kong <zollen.kong@gmail.com>
2018-11-05 19:34:42 -0500

    fix scenarios

--- src/DecisionTreeExercise4.java
+++ src/DecisionTreeExercise4.java
@@ -1,4 +1,4 @@
-public class DecisionTreeExercise3 {
+public class DecisionTreeExercise4 {
 
 	/**
 	 * A book is either a success or failure. If the book is a success, the
@@ -43,9 +43,9 @@ public class DecisionTreeExercise3 {
 			consulted.add(0.46 * 0.54, approved1);
 			consulted.add(0.54 * 0.46, approved2);
 			
-			DecisionTree.Node<ExpectValue> goAhead = DecisionTree.createDecisionNode(new ExpectValue("Go Ahead"));
-			DecisionTree.Node<ExpectValue> success = DecisionTree.createUtilityNode(new ExpectValue(49000d));
-			DecisionTree.Node<ExpectValue> failure = DecisionTree.createUtilityNode(new ExpectValue(-11000d));
+			DecisionTree.Node<ExpectValue> goAhead2 = DecisionTree.createDecisionNode(new ExpectValue("Go Ahead"));
+			DecisionTree.Node<ExpectValue> success2 = DecisionTree.createUtilityNode(new ExpectValue(49000d));
+			DecisionTree.Node<ExpectValue> failure2 = DecisionTree.createUtilityNode(new ExpectValue(-11000d));
 			
 			/**
 			 * P(S=T|R1=T,R2=T) = P(S=T,R1=T,R2=T)/P(R1=T,R2=T)
@@ -54,22 +54,40 @@ public class DecisionTreeExercise3 {
 			 * 					= 0.7 * 0.7 * 0.2 / (0.46 * 0.46)
 			 * 					= 0.463138
 			 * 
-			 * P(S=F|R1=T,R2=T) = P(S=F,R1=T,R2=T)/P(P1=T,R2=T)
+			 * P(S=F|R1=T,R2=T) = P(S=F,R1=T,R2=T)/P(R1=T,R2=T)
 			 * 					= P(R1=T|R2=T,S=F)P(R2=T,S=F)/P(R1=T,R2=T)
 			 * 					= P(R1=T|S=F)P(R2=T|S=F)P(S=F)/(P(R1=T)P(R2=T))
 			 * 					= 0.4 * 0.4 * 0.8 / (0.46 * 0.46)
 			 * 					= 0.60491493
+			 * 
+			 * P(S=T|R1=T,R2=F) = P(S=T,R1=T,R2=F)/P(R1=T,R2=F)
+			 * 					= P(R1=T|R2=F,S=T)P(R2=F,S=T)/P(R1=T,R2=F)
+			 * 					= P(R1=T|S=T)P(R2=F|S=T)P(S=T)/(P(R1=T)P(R2=F))
+			 * 					= 0.7 * 0.3 * 0.2 / (0.46 * 0.54)
+			 * 					= 0.16908213
+			 * 
+			 * P(S=F|R1=T,R2=F) = 1 - P(S=T|R1=T,R2=F) = 0.83091787
 			 */
 			
-			goAhead.add(0.463138d, success);
-			goAhead.add(0.60391493d, failure);
+			goAhead2.add(0.463138d, success2);
+			goAhead2.add(0.60391493d, failure2);
 			
-			bothApproved.add(1.0d, goAhead);
+			bothApproved.add(1.0d, goAhead2);
+			
+			
+			DecisionTree.Node<ExpectValue> goAhead3 = DecisionTree.createDecisionNode(new ExpectValue("Go Ahead"));
+			DecisionTree.Node<ExpectValue> success3 = DecisionTree.createUtilityNode(new ExpectValue(49000d));
+			DecisionTree.Node<ExpectValue> failure3 = DecisionTree.createUtilityNode(new ExpectValue(-11000d));
+			
+			goAhead3.add(0.16908213d, success3);
+			goAhead3.add(0.83091787d, failure3);
+			
+			approved1.add(1.0, goAhead3);
+			approved2.add(1.0, goAhead3);
+			
 			
 			DecisionTree.Node<ExpectValue> loseFee = DecisionTree.createUtilityNode(new ExpectValue(-1000d));
 			bothRejected.add(1.0, loseFee);
-			approved1.add(1.0, loseFee);
-			approved2.add(1.0, loseFee);
 			
 			System.out.println(tree);
 
