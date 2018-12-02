11233e2a6492a67d794473bc5f2b56a13b399240
Stephen Kong <zollen.kong@gmail.com>
2018-12-02 13:58:58 -0500

    fix code

--- src/HiddenMarkovModelExercise1.java
+++ src/HiddenMarkovModelExercise1.java
@@ -1,5 +1,6 @@
 import java.text.DecimalFormat;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -10,7 +11,7 @@ public class HiddenMarkovModelExercise1 {
 
 	private static final DecimalFormat ff = new DecimalFormat("0.######");
 	
-	private static final String[] STATES = { "A", "C", "T", "G" };
+	private static final String[] GENES = { "A", "C", "T", "G" };
 
 	private static DMatrixRMaj T = null;
 	private static DMatrixRMaj E = null;
@@ -67,7 +68,7 @@ public class HiddenMarkovModelExercise1 {
 		// G0 = 0.001645 = T0 * 0.75 * 0.05 + T1 * 0.25 * 0.05
 		// G1 = 0.000473 = T1 * 0.75 * 0.45 + T0 * 0.25 * 0.45
 
-		Map<Integer, Double> starts = new HashMap<Integer, Double>();
+		Map<Integer, Double> starts = new LinkedHashMap<Integer, Double>();
 		starts.put(0, 0.5d);
 		starts.put(1, 0.5d);
 
@@ -97,95 +98,112 @@ public class HiddenMarkovModelExercise1 {
 		// A0 = 0.007313 = c0 * 0.75 * 0.05 + c1 * 0.25 * 0.45 
 		// A1 = 0.018938 = c1 * 0.75 * 0.45 + c0 * 0.25 * 0.05
 		
-		Map<Integer, Double> ends = new HashMap<Integer, Double>();
+		Map<Integer, Double> ends = new LinkedHashMap<Integer, Double>();
 		ends.put(0, 1d);
 		ends.put(1, 1d);
 		
 		backward(ends);
 	}
 	
-	public static void backward(Map<Integer, Double> ends) {
-			
-		Map<Integer, Double> states = new HashMap<Integer, Double>();
-		final Map<Integer, Double> ss = states;
-		
-		ends.entrySet().stream().forEach(p -> ss.put(p.getKey(), p.getValue())); 
-		
-		ss.entrySet().stream().forEach(p -> System.out.println(STATES[STATES.length - 1] + p.getKey() + " ===> " + p.getValue()));
-		
-		for (int gene = STATES.length - 1; gene > 0; gene--) {
-			
-			Map<Integer, Double> nexts = new HashMap<Integer, Double>();
-			Set<Integer> froms = states.keySet();
-			
-			for (Integer from : froms) {
-				
-				double sum = 0d;
-				
-				for (int to = 0; to < T.numRows; to++) {
-				
-					if (T.get(to, from) > 0 && E.get(gene, to) > 0) {
-						
-						double last = 0d;
-						if (states.get(to) != null)
-							last = states.get(to);
-						
-						sum += (double) last * T.get(to, from) * E.get(gene, to);
-					}
-				}
-				
-				System.out.println(STATES[gene - 1] + from + " ===> " + ff.format(sum));
-				nexts.put(from, sum);
-			}
-			
-			states = nexts;
-		}
-	}
-	
 	public static void viterbi() {
 		
 		
 	}
-
-	public static void forward(Map<Integer, Double> starts) {
-
-		Map<Integer, Double> states = new HashMap<Integer, Double>();
-		final Map<Integer, Double> ss = states;
+	
+	public static void backward(Map<Integer, Double> ends) {
+			
+		Map<String, Double> probs = new LinkedHashMap<String, Double>();
+		final Map<String, Double> ss = probs;
 		
-		starts.entrySet().stream().forEach(p -> { 
-				ss.put(p.getKey(), p.getValue() * E.get(0, p.getKey())); 
-		});
+		ends.entrySet().stream().forEach(p -> ss.put(GENES[GENES.length - 1] + String.valueOf(p.getKey()), p.getValue())); 
 		
-		ss.entrySet().stream().forEach(p -> System.out.println(STATES[0] + p.getKey() + " ===> " + p.getValue()));
+		Set<Integer> froms = new LinkedHashSet<Integer>(ends.keySet());
 		
-		
-		for (int gene = 0 + 1; gene < E.numRows; gene++) {
-
-			Map<Integer, Double> nexts = new HashMap<Integer, Double>();
-			Set<Integer> tos = states.keySet();
-		
-			for (Integer to : tos) {
+		for (int gene = GENES.length - 2; gene >= 0; gene--) {
+			
+			Set<Integer> nexts = new LinkedHashSet<Integer>();
+			
+			for (int from : froms) {
 				
-				double sum = 0d;
+				{
+					double sum = 0d;				
+					for (int to = 0; to < T.numRows; to++) {
 				
-				for (int from = 0; from < T.numCols; from++) {
-					
-					if (T.get(to, from) > 0 && E.get(gene, to) > 0) {
-					
-						double last = 0d;
-						if (states.get(from) != null)
-							last = states.get(from);
-					
-						sum += (double) last * T.get(to, from) * E.get(gene, to);
+						if (T.get(to, from) > 0 && E.get(gene, to) > 0) {
+						
+							double last = 0d;
+							if (probs.get(GENES[gene + 1] + String.valueOf(to)) != null)
+								last = probs.get(GENES[gene + 1] + String.valueOf(to));
+						
+							sum += (double) last * T.get(to, from) * E.get(gene + 1, to);
+						}
 					}
+
+					probs.put(GENES[gene] + String.valueOf(from), sum);
 				}
 				
-				System.out.println(STATES[gene] + to + " ===> " + ff.format(sum));
-				nexts.put(to, sum);
+				{
+					for (int _to = 0; _to < T.numRows; _to++) {
+						if (T.get(_to, from) > 0)
+							nexts.add(_to);
+					}	
+				}
+			}
+				
+			froms = nexts;
+		}
+		
+		probs.entrySet().stream().forEach( p -> System.out.println(p.getKey() + " ===> " + ff.format(p.getValue())));
+	}
+	
+	public static void forward(Map<Integer, Double> starts) {
+
+		Map<String, Double> probs = new LinkedHashMap<String, Double>();
+		final Map<String, Double> ss = probs;
+		
+		starts.entrySet().stream().forEach(p -> { 
+				ss.put(GENES[0] + String.valueOf(p.getKey()), p.getValue() * E.get(0, p.getKey())); 
+		});
+		
+		Set<Integer> tos = new LinkedHashSet<Integer>(starts.keySet());
+		
+		for (int gene = 0 + 1; gene < E.numRows; gene++) {
+			
+			Set<Integer> nexts = new LinkedHashSet<Integer>();
+
+			for (int to : tos) {
+				
+				{
+					double sum = 0d;
+					for (int from = 0; from < T.numCols; from++) {
+					
+						if (T.get(to, from) > 0 && E.get(gene, to) > 0) {
+					
+							double last = 0d;
+							if (probs.get(GENES[gene - 1] + String.valueOf(from)) != null)
+								last = probs.get(GENES[gene - 1] + String.valueOf(from));
+					
+							sum += (double) last * T.get(to, from) * E.get(gene, to);
+						}
+					}
+					
+					probs.put(GENES[gene] + String.valueOf(to), sum);
+				}
+				
+				
+				{
+					int from = to;
+					for (int _to = 0; _to < T.numRows; _to++) {
+						if (T.get(_to, from) > 0)
+							nexts.add(_to);
+					}
+				}
 			}
 			
-			states = nexts;
+			tos = nexts;
 		}
+		
+		probs.entrySet().stream().forEach( p -> System.out.println(p.getKey() + " ===> " + ff.format(p.getValue())));
 	}
 
 }
