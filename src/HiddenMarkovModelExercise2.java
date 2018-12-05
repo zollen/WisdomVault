f54253c2e46a8a965a5747ea94e5f674173c9fc7
Stephen Kong <zollen.kong@gmail.com>
2018-12-04 21:03:22 -0500

    improve viterbi for HMM

--- src/HiddenMarkovModelExercise2.java
+++ src/HiddenMarkovModelExercise2.java
@@ -1,7 +1,7 @@
 import java.text.DecimalFormat;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -12,6 +12,14 @@ public class HiddenMarkovModelExercise2 {
 	
 	private static final DecimalFormat ff = new DecimalFormat("0.######");
 	
+	private static DMatrixRMaj T = null;
+	private static DMatrixRMaj E = null;	
+	
+	
+	private static final String [] STATES = { "1", "2" };
+	
+	private static final String [] SEQUENCE = { "b", "a", "d" };
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -21,8 +29,8 @@ public class HiddenMarkovModelExercise2 {
 		 * start_probability = {'1': 0.6, '2': 0.4}
 		 * 
 		 * transition_probability = { 
-		 * 				'1' : {'start': 0.6, '1': 0.5, '2': 0.3}, 
-		 * 				'2' : {'start': 0.4, '1': 0.5, '2': 0.7}
+		 * 				'1' : {'start': 0.6, '1': 0.5, '2': 0.5}, 
+		 * 				'2' : {'start': 0.4, '1': 0.3, '2': 0.7}
 		 * }
 		 * 
 		 * emission_probability = { 
@@ -33,151 +41,80 @@ public class HiddenMarkovModelExercise2 {
 		
 		Equation eq = new Equation();		 
 		eq.process("T = [" +
-						/*   S,   b1,   b2,   a1,   a2,   d1,   d2 */
-			/* S  */    "  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0;" +
-			/* b1 */	"  0.6,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0;" +
-			/* b2 */	"  0.4,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0;" +
-			/* a1 */	"  0.0, 0.20, 0.12,  0.0,  0.0,  0.0,  0.0;" +
-			/* a2 */	"  0.0, 0.15, 0.21,  0.0,  0.0,  0.0,  0.0;" +
-			/* d1 */    "  0.0,  0.0,  0.0, 0.05, 0.03,  0.0,  0.0;" +
-			/* d2 */    "  0.0,  0.0,  0.0, 0.10, 0.14,  0.0,  0.0 " +
+					/*      1,   2     */
+				/* 1 */	" 0.5, 0.3;" +
+				/* 2 */	" 0.5, 0.7 " +
+						"]");
+		
+		eq.process("E = [" +
+					/*      1,    2    */
+				/* a */	" 0.4,  0.3;" +
+				/* b */	" 0.3,  0.2;" +
+				/* c */	" 0.2,  0.3;" +
+				/* d */	" 0.1,  0.2 " +	
 					"]");
-		
-		Set<Integer> starts = new HashSet<Integer>(); 
-		starts.add(0);
-		
-		Map<Integer, Arc> results = new LinkedHashMap<Integer, Arc>();
-		DMatrixRMaj T = eq.lookupDDRM("T");
 	
-		viterbi(T, starts, results);
 		
-		results.entrySet().stream().forEach(p -> System.out.println(Arc.STATES[p.getKey()] + " ===> " + p.getValue()));
+		T = eq.lookupDDRM("T");
+		E = eq.lookupDDRM("E");
 		
-		
-		Set<Integer> ends = new HashSet<Integer>(); 
-		ends.add(5);
-		ends.add(6);
-		
-		StringBuffer buffer = new StringBuffer();
-		traverse(results, ends, buffer);
-		
-		System.out.println(buffer.toString());
+		Map<Integer, Double> starts = new HashMap<Integer, Double>(); 
+		starts.put(0, 0.6d);
+		starts.put(1, 0.4d);
+	
+		viterbi(starts, SEQUENCE);
 	}
 	
-	public static void viterbi(DMatrixRMaj graph, Collection<Integer> current, Map<Integer, Arc> results) {
+	public static void viterbi(Map<Integer, Double> starts, String [] sequence) {
 		
-		Collection<Integer> nexts = new HashSet<Integer>();
+		Map<String, Double> probs = new LinkedHashMap<String, Double>();
+		final Map<String, Double> ss = probs;
 		
-		if (current.isEmpty())
-			return;
+		starts.entrySet().stream().forEach(
+				p -> { 
+					ss.put("0" + "#" + STATES[p.getKey()] + sequence[0], p.getValue() * E.get(0, p.getKey()));
+				}
+		);
 		
-		for (int from : current) {
+		Set<Integer> froms = new LinkedHashSet<Integer>(starts.keySet());
+		Set<Integer> nexts = new LinkedHashSet<Integer>();
 		
-			for (int to = 0; to < graph.numRows; to++) {
+		for (int step = 1; step < sequence.length; step++) {
+			
+			for (int from : froms) {
 				
-				double cost = 0.0d;
-				if (to < graph.numRows && from < graph.numCols)
-					cost = (double) graph.get(to, from);
-				
-				if (cost > 0) {
+				for (int to = 0; to < T.numRows; to++) {
 					
-					Arc src = results.get(from);
-					Arc dest = results.get(to);
-					
-					double total = 0d;
-					if (src != null)
-						total = src.getProb();
-					
-					if (dest == null || (dest != null && dest.getProb() < total + cost)) {
-						results.put(to, new Arc(from, to, total + cost));
+					if (T.get(to, from) > 0 && E.get(step, to) > 0) {
+						
+						double left = 0.0d;
+						double right = 0.0d;
+						if (ss.get((step - 1) + "#" + STATES[from] + sequence[step - 1]) != null) {
+							left = ss.get((step - 1) + "#" + STATES[from] + sequence[step - 1]);
+						}
+						
+						if (ss.get(step + "#" + STATES[to] + sequence[step]) != null) {
+							right = ss.get(step + "#" + STATES[to] + sequence[step]);
+						}
+						
+						if (left > 0) {							
+							left = left * T.get(to, from) * E.get(step, to);
+							
+							if (left > right)
+								ss.put(step + "#" + STATES[to] + sequence[step], left);
+						}	
+						
 						nexts.add(to);
 					}
 				}
 			}
+			
+			froms = new LinkedHashSet<Integer>(nexts);
+			nexts.clear();
 		}
 		
-		viterbi(graph, nexts, results);
 		
+		ss.entrySet().stream().forEach(p -> System.out.println(p.getKey() + " ==> " + ff.format(p.getValue())));
 	}
 	
-	public static void traverse(Map<Integer, Arc> results, Collection<Integer> current, StringBuffer output) {
-		
-		if (current.isEmpty())
-			return;
-		
-		double val = Double.MIN_VALUE;
-		int selected = -1;
-		
-		for (int j : current) {
-			
-			Arc arc = results.get(j);
-			
-			if (arc != null && arc.getProb() > val) {
-				val = arc.getProb();
-				selected = j;
-			}
-			else 
-			if (arc == null) {
-				selected = j;
-			}
-		}
-		
-		
-		Arc arc = results.get(selected);
-		if (arc != null) {
-			
-			Collection<Integer> nexts = new HashSet<Integer>();
-			nexts.add(arc.getFrom());
-			
-			
-			traverse(results, nexts, output);
-			
-			output.append(" ==> " + Arc.STATES[selected]);
-		}
-		else {
-			output.append(Arc.STATES[selected]);
-		}
-	}
-	
-	
-	public static class Arc {
-		
-		public static final String [] STATES = { "S", "b1", "b2", "a1", "a2", "d1", "d2" };
-
-		
-		private int from = -1;
-		private int to = -1;
-		private double prob = -1;
-		
-		public Arc() {}
-		
-		public Arc(int from, int to, double cost) {
-			this.from = from;
-			this.to = to;
-			this.prob = cost;
-		}
-		
-		public int getFrom() {
-			return from;
-		}
-		public void setFrom(int from) {
-			this.from = from;
-		}
-		public int getTo() {
-			return to;
-		}
-		public void setTo(int to) {
-			this.to = to;
-		}
-		public double getProb() {
-			return prob;
-		}
-		public void setProb(double cost) {
-			this.prob = cost;
-		}
-		@Override
-		public String toString() {
-			return "Arc [from=" + STATES[from] + ", to=" + STATES[to] + ", prob=" + ff.format(prob) + "]";
-		}
-	}
 }
