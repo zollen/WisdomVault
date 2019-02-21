1ddcbb044d8e6bcd720b14e27c2bc135d339b3e8
Stephen Kong <zollen.kong@gmail.com>
2019-02-20 20:01:13 -0500

    works

--- src/LinearProgramming2.java
+++ src/LinearProgramming2.java
@@ -1,12 +1,17 @@
 import java.text.DecimalFormat;
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
-import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
-import org.apache.commons.math3.distribution.RealDistribution;
-import org.apache.commons.math3.distribution.UniformRealDistribution;
-import org.apache.commons.math3.fitting.PolynomialCurveFitter;
-import org.apache.commons.math3.fitting.WeightedObservedPoints;
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresBuilder;
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresOptimizer;
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresProblem;
+import org.apache.commons.math3.fitting.leastsquares.LevenbergMarquardtOptimizer;
+import org.apache.commons.math3.fitting.leastsquares.MultivariateJacobianFunction;
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.util.Pair;
 
 public class LinearProgramming2 {
 
@@ -15,46 +20,60 @@ public class LinearProgramming2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// unkown high degree polynomial with single unknown: x
-		// 3 + 5 x + 4 x^2 - 3 x^3 + 2 x^4
-		
-		// we have a collection of random samples (i.e. points)
-		// we want to guess the coefficient of the polynomial equation
-		
-		final RealDistribution rng = new UniformRealDistribution(-100, 100);
-        rng.reseedRandomGenerator(System.nanoTime());
-		
-		
-		final double[] coeff = { 3, 5, 4, -3, 2 }; // 3 + 5 x + 4 x^2 - 3 x^3 + 2 x^4
-        final PolynomialFunction f = new PolynomialFunction(coeff);
+		final double radius = 70.0;
+		  final Vector2D[] observedPoints = new Vector2D[] {
+		      new Vector2D( 30.0,  68.0),
+		      new Vector2D( 50.0,  -6.0),
+		      new Vector2D(110.0, -20.0),
+		      new Vector2D( 35.0,  15.0),
+		      new Vector2D( 45.0,  97.0)
+		  };
 
-        // Collect data from a known polynomial.
-        final WeightedObservedPoints obs = new WeightedObservedPoints();
-        for (int i = 0; i < 100; i++) {
-            final double x = rng.sample();
-            obs.add(x, f.value(x));
-        }
+		  // the model function components are the distances to current estimated center,
+		  // they should be as close as possible to the specified radius
+		  MultivariateJacobianFunction distancesToCurrentCenter = new MultivariateJacobianFunction() {
+		      public Pair<RealVector, RealMatrix> value(final RealVector point) {
 
-        // Start fit from initial guesses that are far from the optimal values.
-        final PolynomialCurveFitter fitter
-            = PolynomialCurveFitter
-            		.create(4)
-            		.withStartPoint(new double[] { 1, 5, 10, 100, 500  });
-        
-        // other one variable CurveFitter:
-        // 1. SimpleCurveFitter
-        // 2. HarmonicCurveFitter
-        // 3. GaussianCurveFitter (for curves of normal distribution)
-        
-        double[] best = fitter.fit(obs.toList());
-		
-       
-        System.out.println(Arrays.stream(coeff).mapToObj(p -> ff.format(p)).collect(Collectors.joining(", ")));
- 
-        System.out.println("Calculating the best fit should be equal to coeff!!");
-        
-        System.out.println(Arrays.stream(best).mapToObj(p -> ff.format(p)).collect(Collectors.joining(", ")));
+		          Vector2D center = new Vector2D(point.getEntry(0), point.getEntry(1));
 
+		          RealVector value = new ArrayRealVector(observedPoints.length);
+		          RealMatrix jacobian = new Array2DRowRealMatrix(observedPoints.length, 2);
+
+		          for (int i = 0; i < observedPoints.length; ++i) {
+		              Vector2D o = observedPoints[i];
+		              double modelI = Vector2D.distance(o, center);
+		              value.setEntry(i, modelI);
+		              // derivative with respect to p0 = x center
+		              jacobian.setEntry(i, 0, (center.getX() - o.getX()) / modelI);
+		              // derivative with respect to p1 = y center
+		              jacobian.setEntry(i, 1, (center.getX() - o.getX()) / modelI);
+		          }
+
+		          return new Pair<RealVector, RealMatrix>(value, jacobian);
+
+		      }
+		  };
+
+		  // the target is to have all points at the specified radius from the center
+		  double[] prescribedDistances = new double[observedPoints.length];
+		  Arrays.fill(prescribedDistances, radius);
+
+		  // least squares problem to solve : modeled radius should be close to target radius
+		  LeastSquaresProblem problem = new LeastSquaresBuilder().
+		                                start(new double[] { 100.0, 50.0 }).
+		                                model(distancesToCurrentCenter).
+		                                target(prescribedDistances).
+		                                lazyEvaluation(false).
+		                                maxEvaluations(1000).
+		                                maxIterations(1000).
+		                                build();
+		  LeastSquaresOptimizer.Optimum optimum = new LevenbergMarquardtOptimizer().optimize(problem);
+		  Vector2D fittedCenter = new Vector2D(optimum.getPoint().getEntry(0), optimum.getPoint().getEntry(1));
+		  System.out.println("fitted center: [" + ff.format(fittedCenter.getX()) + ", " + 
+				  						ff.format(fittedCenter.getY()) + "]");
+		  System.out.println("RMS: "           + ff.format(optimum.getRMS()));
+		  System.out.println("evaluations: "   + optimum.getEvaluations());
+		  System.out.println("iterations: "    + optimum.getIterations());
 	}
 
 }
---
+++ src/LinearProgramming3.java
@@ -0,0 +1,60 @@
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
+import org.apache.commons.math3.distribution.RealDistribution;
+import org.apache.commons.math3.distribution.UniformRealDistribution;
+import org.apache.commons.math3.fitting.PolynomialCurveFitter;
+import org.apache.commons.math3.fitting.WeightedObservedPoints;
+
+public class LinearProgramming3 {
+
+	private static final DecimalFormat ff = new DecimalFormat("#,##0.000");
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// unkown high degree polynomial with single unknown: x
+		// 3 + 5 x + 4 x^2 - 3 x^3 + 2 x^4
+		
+		// we have a collection of random samples (i.e. points)
+		// we want to guess the coefficient of the polynomial equation
+		
+		final RealDistribution rng = new UniformRealDistribution(-100, 100);
+        rng.reseedRandomGenerator(System.nanoTime());
+		
+		
+		final double[] coeff = { 3, 5, 4, -3, 2 }; // 3 + 5 x + 4 x^2 - 3 x^3 + 2 x^4
+        final PolynomialFunction f = new PolynomialFunction(coeff);
+
+        // Collect data from a known polynomial.
+        final WeightedObservedPoints obs = new WeightedObservedPoints();
+        for (int i = 0; i < 100; i++) {
+            final double x = rng.sample();
+            obs.add(x, f.value(x));
+        }
+
+        // Start fit from initial guesses that are far from the optimal values.
+        final PolynomialCurveFitter fitter
+            = PolynomialCurveFitter
+            		.create(4)
+            		.withStartPoint(new double[] { 1, 5, 10, 100, 500  });
+        
+        // other one variable CurveFitter:
+        // 1. SimpleCurveFitter
+        // 2. HarmonicCurveFitter
+        // 3. GaussianCurveFitter (for curves of normal distribution)
+        
+        double[] best = fitter.fit(obs.toList());
+		
+       
+        System.out.println(Arrays.stream(coeff).mapToObj(p -> ff.format(p)).collect(Collectors.joining(", ")));
+ 
+        System.out.println("Calculating the best fit should be equal to coeff!!");
+        
+        System.out.println(Arrays.stream(best).mapToObj(p -> ff.format(p)).collect(Collectors.joining(", ")));
+
+	}
+
+}
