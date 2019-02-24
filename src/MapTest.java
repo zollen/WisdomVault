8c1ae34aa9adef008f48a4ae7ffbed33951c3682
Stephen Kong <zollen.kong@gmail.com>
2019-02-24 17:01:08 -0500

    simplify coding

--- src/MapTest.java
+++ src/MapTest.java
@@ -10,26 +10,26 @@ public class MapTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 		// company_cd
-
-		// request_type: inquiry
-
-		// request_type: order
+		// request_type: order or inquiry
 		// transaction_type 
 		// transaction_type_detail
 		// action_code
 		// account_section
 
-		register("2400", "*", "*", "*", "*", "DAMN");
-		register("2400", "5", "*", "1", "*", "COOL");
-		register("2400", "5", "5", "0", "L", "API");
-//		register("2400", "5", "5", "1", "*", "NONE");
-		register("2400", "5", "5", "1", "L", "COPY");
+		register("2400", "I", "*", "*", "*", "*", "NUTS");
+		register("2400", "O", "*", "*", "*", "*", "DAMN");
+		register("2400", "O", "5", "*", "1", "*", "COOL");
+		register("2400", "O", "5", "5", "0", "L", "API");
+//		register("2400", "O", "5", "5", "1", "*", "NONE");
+		register("2400", "O", "5", "5", "1", "L", "COPY");
 	
-		System.out.println(search("2400", "5", "9", "0", "*"));
-		System.out.println(search("2400", "5", "5", "0", "L"));
-		System.out.println(search("2400", "5", "5", "1", "K"));
-		System.out.println(search("2400", "5", "5", "1", "L"));
+		System.out.println(search("2400", "I", "*", "*", "*", "*"));
+		System.out.println(search("2400", "O", "5", "9", "0", "*"));
+		System.out.println(search("2400", "O", "5", "5", "0", "L"));
+		System.out.println(search("2400", "O", "5", "5", "1", "K"));
+		System.out.println(search("2400", "O", "5", "5", "1", "L"));
 		
 //		map.entrySet().stream().forEach(p -> System.out.println(p.getKey() + " ==> " + p.getValue()));
 	}
@@ -39,67 +39,94 @@ public class MapTest {
 		List<String> combos = new ArrayList<String>();
 		
 		
-		combos.add("N:" + tokens[0] + ":" + tokens[1] + tokens[2] + tokens[3] + "*");
-		combos.add("N:" + tokens[0] + ":" + tokens[1] + tokens[2] + "*" + tokens[4]);
-		combos.add("N:" + tokens[0] + ":" + tokens[1] + tokens[2] + "*" + "*");
+		combos.add("N:" + tokens[0] + ":O:" + tokens[1] + tokens[2] + tokens[3] + "*");
+		combos.add("N:" + tokens[0] + ":O:" + tokens[1] + tokens[2] + "*" + tokens[4]);
+		combos.add("N:" + tokens[0] + ":O:" + tokens[1] + tokens[2] + "*" + "*");
 		
-		combos.add("N:" + tokens[0] + ":" + tokens[1] + "*" + tokens[3] + tokens[4]);
-		combos.add("N:" + tokens[0] + ":" + tokens[1] + "*" + tokens[3] + "*");
-		combos.add("N:" + tokens[0] + ":" + tokens[1] + "*" + "*" + tokens[4]);
-		combos.add("N:" + tokens[0] + ":" + tokens[1] + "*" + "*" + "*");
+		combos.add("N:" + tokens[0] + ":O:" + tokens[1] + "*" + tokens[3] + tokens[4]);
+		combos.add("N:" + tokens[0] + ":O:" + tokens[1] + "*" + tokens[3] + "*");
+		combos.add("N:" + tokens[0] + ":O:" + tokens[1] + "*" + "*" + tokens[4]);
+		combos.add("N:" + tokens[0] + ":O:" + tokens[1] + "*" + "*" + "*");
 		
-		combos.add("N:" + tokens[0] + ":" + "*" + tokens[2] + tokens[3] + tokens[4]);
-		combos.add("N:" + tokens[0] + ":" + "*" + tokens[2] + "*" + tokens[4]);
-		combos.add("N:" + tokens[0] + ":" + "*" + tokens[2] + tokens[3] + "*");
-		combos.add("N:" + tokens[0] + ":" + "*" + tokens[2] + "*" + "*");
+		combos.add("N:" + tokens[0] + ":O:" + "*" + tokens[2] + tokens[3] + tokens[4]);
+		combos.add("N:" + tokens[0] + ":O:" + "*" + tokens[2] + "*" + tokens[4]);
+		combos.add("N:" + tokens[0] + ":O:" + "*" + tokens[2] + tokens[3] + "*");
+		combos.add("N:" + tokens[0] + ":O:" + "*" + tokens[2] + "*" + "*");
 		
-		combos.add("N:" + tokens[0] + ":" + "*" + "*" + tokens[3] + tokens[4]);
-		combos.add("N:" + tokens[0] + ":" + "*" + "*" + "*" + tokens[4]);
-		combos.add("N:" + tokens[0] + ":" + "*" + "*" + tokens[3] + "*");
-		combos.add("N:" + tokens[0] + ":" + "*" + "*" + "*" + "*");
+		combos.add("N:" + tokens[0] + ":O:" + "*" + "*" + tokens[3] + tokens[4]);
+		combos.add("N:" + tokens[0] + ":O:" + "*" + "*" + "*" + tokens[4]);
+		combos.add("N:" + tokens[0] + ":O:" + "*" + "*" + tokens[3] + "*");
+		combos.add("N:" + tokens[0] + ":O:" + "*" + "*" + "*" + "*");
+		
+		combos.add("N:ALL:O:" + tokens[1] + tokens[2] + tokens[3] + "*");
+		combos.add("N:ALL:O:" + tokens[1] + tokens[2] + "*" + tokens[4]);
+		combos.add("N:ALL:O:" + tokens[1] + tokens[2] + "*" + "*");
+		
+		combos.add("N:ALL:O:" + tokens[1] + "*" + tokens[3] + tokens[4]);
+		combos.add("N:ALL:O:" + tokens[1] + "*" + tokens[3] + "*");
+		combos.add("N:ALL:O:" + tokens[1] + "*" + "*" + tokens[4]);
+		combos.add("N:ALL:O:" + tokens[1] + "*" + "*" + "*");
+		
+		combos.add("N:ALL:O:" + "*" + tokens[2] + tokens[3] + tokens[4]);
+		combos.add("N:ALL:O:" + "*" + tokens[2] + "*" + tokens[4]);
+		combos.add("N:ALL:O:" + "*" + tokens[2] + tokens[3] + "*");
+		combos.add("N:ALL:O:" + "*" + tokens[2] + "*" + "*");
+		
+		combos.add("N:ALL:O:" + "*" + "*" + tokens[3] + tokens[4]);
+		combos.add("N:ALL:O:" + "*" + "*" + "*" + tokens[4]);
+		combos.add("N:ALL:O:" + "*" + "*" + tokens[3] + "*");
+		combos.add("N:ALL:O:" + "*" + "*" + "*" + "*");
 		
 		
 		return combos.toArray(new String[0]);
 	}
 	
+	public static void register(String code, String rtyp, String tp, String tpd, String ac, String as, String mode) {
+		
+		map.put("N:" + code + ":" + rtyp + ":" + tp + tpd + ac + as, mode);
+	}
+	
+	public static String search(String code, String rtyp, String tp, String tpd, String ac, String as) {
+		
+		if ("I".equals(rtyp))
+			tp = tpd = ac = as = "*";
+		
+		String[] tokens = { code, tp, tpd, ac, as };
+		
+		String val = map.get("N:" + code + ":" + rtyp + ":" + tp + tpd + ac + as);
+		
+		if (val == null) {
+			
+			String [] keys = queries(tokens);
+			for (int i = 0; i < keys.length; i++) {
+				
+				val = map.get(keys[i]);
+				if (val != null)
+					break;	
+			}
+		}
+		
+		return val;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/*
+ 
 	public static void register(String code, String tp, String tpd, String ac, String as, String mode) {
 		
 		String[] keys = { code, tp, tpd, ac, as };
 		
 		_register("N:" + code + ":", 1, keys, mode);
-	}
-	
-	public static void _register(String prefix, int index, String [] tokens, String mode) {
-		
-		if (index == tokens.length) {
-			
-			if (prefix.equals("N:" + tokens[0] + ":" + tokens[1] + tokens[2] + 
-					tokens[3] + tokens[4])) {
-				map.put(prefix, "L:" + mode);
-			}
-
-			return;		
-		}
-		
-		String val = map.get(prefix);
-		
-		if (val == null) {
-			
-			map.put(prefix, prefix + "*");
-			_register(prefix + "*", index + 1, tokens, mode);
-			
-			if (!"*".equals(tokens[index])) {
-				map.put(prefix, prefix + "*" + "," + prefix + tokens[index]);
-				_register(prefix + tokens[index], index + 1, tokens, mode);
-			}		
-		}
-		else {
-			
-			if (val.indexOf(prefix + tokens[index]) < 0)
-				map.put(prefix, val + "," + prefix + tokens[index]);
-			
-			_register(prefix + tokens[index], index + 1, tokens, mode);	
-		}
 	}
 	
 	public static String search(String code, String tp, String tpd, String ac, String as) {
@@ -146,4 +173,38 @@ public class MapTest {
 		
 		return val;
 	}
+	
+	public static void _register(String prefix, int index, String [] tokens, String mode) {
+		
+		if (index == tokens.length) {
+			
+			if (prefix.equals("N:" + tokens[0] + ":" + tokens[1] + tokens[2] + 
+					tokens[3] + tokens[4])) {
+				map.put(prefix, "L:" + mode);
+			}
+
+			return;		
+		}
+		
+		String val = map.get(prefix);
+		
+		if (val == null) {
+			
+			map.put(prefix, prefix + "*");
+			_register(prefix + "*", index + 1, tokens, mode);
+			
+			if (!"*".equals(tokens[index])) {
+				map.put(prefix, prefix + "*" + "," + prefix + tokens[index]);
+				_register(prefix + tokens[index], index + 1, tokens, mode);
+			}		
+		}
+		else {
+			
+			if (val.indexOf(prefix + tokens[index]) < 0)
+				map.put(prefix, val + "," + prefix + tokens[index]);
+			
+			_register(prefix + tokens[index], index + 1, tokens, mode);	
+		}
+	}
+*/
 }
