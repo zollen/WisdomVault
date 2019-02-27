19263c3a6c04d199f7139478dac3604aa2eb0959
Stephen Kong <zollen.kong@gmail.com>
2019-02-26 20:11:58 -0500

    cubic splines

--- src/LinearProgramming5.java
+++ src/LinearProgramming5.java
@@ -1,3 +1,4 @@
+import org.apache.commons.math3.analysis.UnivariateFunction;
 import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
 import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
@@ -8,25 +9,37 @@ public class LinearProgramming5 {
 		// TODO Auto-generated method stub
 		
         // The following polynomial function is for verify the interpolated result.
-		final double[] coeff = { 3, 5, 4 }; // 3 + 5 x + 4 x^2
+		final double[] coeff = { 3, 5, 4, -3, 2 }; // 3 + 5 x + 4 x^2 - 3 x^3 + 2 x^4
         final PolynomialFunction P = new PolynomialFunction(coeff); // or we have a collection of observed points
 
-        double [] x = new double[10];
-        double [] y = new double[10];
-        for (int i = 0; i < 10; i++) {
+        double [] x = new double[100];
+        double [] y = new double[100];
+        for (int i = 0; i < 100; i++) {
             x[i] = i;
             y[i] = P.value(x[i]);  // It could be replaced with any observed (x, y)
         }
         
         SplineInterpolator interpolator = new SplineInterpolator();
         
-        PolynomialSplineFunction  f = interpolator.interpolate(x, y);
+        UnivariateFunction  f = interpolator.interpolate(x, y);
         
-        PolynomialFunction polynomials[] = f.getPolynomials();
+        System.out.println("P(5) = 3 + 5(5) + 4(5)^2 - 3(5)^3 + 2(5)^4 = 1003");
+        System.out.println("f(5): " + f.value(5.5));
         
-       for (int i = 0; i < polynomials.length; i++) {
-    	   System.out.println(i + " => " + (i + 1) + " of cubic splines : " + polynomials[i]);  
-       }
+        System.out.println("\nLet's predict an unknown point between measurements");
+        // Result between points x = 5 and x = 6
+        System.out.println("P(5.5) = 3 + 5(5.5) + 4(5.5)^2 - 3(5.5)^3 + 2(5.5)^4 = 1482.5");
+        System.out.println("f(5.5): " + f.value(5.5));
+        
+        
+        
+        PolynomialSplineFunction p = interpolator.interpolate(x, y);
+        
+        PolynomialFunction ff [] = p.getPolynomials();
+        
+        for (int i = 0; i < 10; i++) {
+        	System.out.println("[" + i + "] " + ff[i] + " = " + ff[i].value(0));
+        }
  
 	}
 
