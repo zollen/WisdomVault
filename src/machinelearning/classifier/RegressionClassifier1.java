acb7b254629d9277db02f73ff39358521ea5b69a
Stephen Kong <zollen.kong@gmail.com>
2019-04-07 14:34:50 -0400

    weka LDA

--- src/machinelearning/classifier/RegressionClassifier1.java
+++ src/machinelearning/classifier/RegressionClassifier1.java
@@ -1,6 +1,5 @@
 package machinelearning.classifier;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import weka.core.Attribute;
@@ -10,140 +9,108 @@ import weka.core.Instances;
 
 public class RegressionClassifier1 {
 	
-	private static final DecimalFormat ff = new DecimalFormat("0.000");
-	
-	private static final String VALUE_COLOR_GREEN = "Green";
-	private static final String VALUE_COLOR_BLUE = "Blue";
-	private static final String VALUE_COLOR_RED = "Red";
-	
-	private static final String VALUE_GENDER_MALE = "Male";
-	private static final String VALUE_GENDER_FEMALE = "Female";
-	
-	private static final double LEARNING_RATE = 0.1;
+	private static final String VALUE_CLASS_DOG = "Dog";
+	private static final String VALUE_CLASS_CAT = "Cat";
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		ArrayList<String> genderVals = new ArrayList<String>();
-		genderVals.add(VALUE_GENDER_MALE);
-		genderVals.add(VALUE_GENDER_FEMALE);
+		ArrayList<String> clsVals = new ArrayList<String>();
+		clsVals.add(VALUE_CLASS_DOG);
+		clsVals.add(VALUE_CLASS_CAT);
 		
-		ArrayList<String> colorVals = new ArrayList<String>();
-		colorVals.add(VALUE_COLOR_GREEN);
-		colorVals.add(VALUE_COLOR_BLUE);
-		colorVals.add(VALUE_COLOR_RED);
-		
-		
+
 		ArrayList<Attribute> attrs = new ArrayList<Attribute>();
-		Attribute attr1 = new Attribute("height", 1);
-		Attribute attr2 = new Attribute("color", colorVals, 2);
-		Attribute attr3 = new Attribute("gender", genderVals, 3);
-		Attribute attr4 = new Attribute("weight", 4);
-		
+		Attribute attr1 = new Attribute("weight", 1);
+		Attribute attr2 = new Attribute("height", 2);
+		Attribute attr3 = new Attribute("animal", clsVals, 3);
 		attrs.add(attr1);
 		attrs.add(attr2);
 		attrs.add(attr3);
-		attrs.add(attr4);
-		
 		
 		
 		Instances training = generateTrainingData(attrs);
 
-	
+		LDA classifier = new LDA();
+		
+		classifier.setDebug(true);
+		classifier.buildClassifier(training);
+		
+		System.out.println(classifier.globalInfo());
+		System.out.println(classifier);
 		
 		// prediction
-		Instances testing = generateTestingData(attrs);
+		Instances testing = generateTestData(attrs);
 		
 		
-		
-		System.out.println("Predicting(1.7, Green, Female) ===> " );
-		
-	
-		System.out.println("Predicting(1.53, Red, Male) ===> " );	
-		
-		
-		System.out.println("Predicting(1.44, Blue, Male) ===> " );
+		for (Instance instance : testing)
+			System.out.println(instance + " ==> " + classifier.classifyInstance(instance));
 		
 	}
 	
-	public static Instances generateTestingData(ArrayList<Attribute> attrs) {
+	public static Instances generateTestData(ArrayList<Attribute> attrs) {
 		
-		Instances testing = new Instances("TESTING", attrs, 3);
-		Instance test1 = new DenseInstance(4);	
-		test1.setValue(attrs.get(0), 1.7);
-		test1.setValue(attrs.get(1), VALUE_COLOR_GREEN);
-		test1.setValue(attrs.get(2), VALUE_GENDER_FEMALE);
-		test1.setValue(attrs.get(3), 75.63);
-		testing.add(test1);
+		Instances testing = new Instances("TESTING", attrs, 2);
 		
-		Instance test2 = new DenseInstance(4);	
-		test2.setValue(attrs.get(0), 1.53);
-		test2.setValue(attrs.get(1), VALUE_COLOR_RED);
-		test2.setValue(attrs.get(2), VALUE_GENDER_MALE);
-		test2.setValue(attrs.get(3), 72.31);
-		testing.add(test2);
+		Instance data1 = new DenseInstance(3);	
+		data1.setValue(attrs.get(0), 8.5);
+		data1.setValue(attrs.get(1), 13);
+	//	data1.setValue(attrs.get(2), VALUE_CLASS_CAT);
+		testing.add(data1);
 		
-		Instance test3 = new DenseInstance(4);	
-		test3.setValue(attrs.get(0), 1.44);
-		test3.setValue(attrs.get(1), VALUE_COLOR_BLUE);
-		test3.setValue(attrs.get(2), VALUE_GENDER_MALE);
-		test3.setValue(attrs.get(3), 73.11);
-		testing.add(test3);
+		Instance data2 = new DenseInstance(3);	
+		data2.setValue(attrs.get(0), 11);
+		data2.setValue(attrs.get(1), 7.6);
+	//	data2.setValue(attrs.get(2), VALUE_CLASS_DOG);
+		testing.add(data2);
 		
 		testing.setClassIndex(testing.numAttributes() - 1);
 		
 		return testing;
 	}
-	
-	
+
 	public static Instances generateTrainingData(ArrayList<Attribute> attrs) {
 
-		Instances training = new Instances("TRAINING", attrs, 6);
+		Instances training = new Instances("TRAINING", attrs, 5);
 		
-		Instance data1 = new DenseInstance(4);	
-		data1.setValue(attrs.get(0), 1.6);
-		data1.setValue(attrs.get(1), VALUE_COLOR_BLUE);
-		data1.setValue(attrs.get(2), VALUE_GENDER_MALE);
-		data1.setValue(attrs.get(3), 88);
+		// ANSWERS:
+		// weight <= 12.0
+		//    + - true  - height <= 8.5
+		//					+ - dog (1)
+		//					+ - cat (2)
+		//    + - false - dog (2)
+		
+		Instance data1 = new DenseInstance(3);	
+		data1.setValue(attrs.get(0), 8);
+		data1.setValue(attrs.get(1), 8);
+		data1.setValue(attrs.get(2), VALUE_CLASS_DOG);
 		training.add(data1);
 		
-		Instance data2 = new DenseInstance(4);	
-		data2.setValue(attrs.get(0), 1.6);
-		data2.setValue(attrs.get(1), VALUE_COLOR_GREEN);
-		data2.setValue(attrs.get(2), VALUE_GENDER_FEMALE);
-		data2.setValue(attrs.get(3), 76);
+		Instance data2 = new DenseInstance(3);	
+		data2.setValue(attrs.get(0), 50);
+		data2.setValue(attrs.get(1), 40);
+		data2.setValue(attrs.get(2), VALUE_CLASS_DOG);
 		training.add(data2);
 		
-		Instance data3 = new DenseInstance(4);	
-		data3.setValue(attrs.get(0), 1.5);
-		data3.setValue(attrs.get(1), VALUE_COLOR_BLUE);
-		data3.setValue(attrs.get(2), VALUE_GENDER_FEMALE);
-		data3.setValue(attrs.get(3), 56);
+		Instance data3 = new DenseInstance(3);	
+		data3.setValue(attrs.get(0), 8);
+		data3.setValue(attrs.get(1), 9);
+		data3.setValue(attrs.get(2), VALUE_CLASS_CAT);
 		training.add(data3);
 		
-		Instance data4 = new DenseInstance(4);	
-		data4.setValue(attrs.get(0), 1.8);
-		data4.setValue(attrs.get(1), VALUE_COLOR_RED);
-		data4.setValue(attrs.get(2), VALUE_GENDER_MALE);
-		data4.setValue(attrs.get(3), 73);
+		Instance data4 = new DenseInstance(3);	
+		data4.setValue(attrs.get(0), 15);
+		data4.setValue(attrs.get(1), 12);
+		data4.setValue(attrs.get(2), VALUE_CLASS_DOG);
 		training.add(data4);
 		
-		Instance data5 = new DenseInstance(4);	
-		data5.setValue(attrs.get(0), 1.5);
-		data5.setValue(attrs.get(1), VALUE_COLOR_GREEN);
-		data5.setValue(attrs.get(2), VALUE_GENDER_MALE);
-		data5.setValue(attrs.get(3), 77);
+		Instance data5 = new DenseInstance(3);	
+		data5.setValue(attrs.get(0), 9);
+		data5.setValue(attrs.get(1), 10);
+		data5.setValue(attrs.get(2), VALUE_CLASS_CAT);
 		training.add(data5);
 		
-		Instance data6 = new DenseInstance(4);	
-		data6.setValue(attrs.get(0), 1.4);
-		data6.setValue(attrs.get(1), VALUE_COLOR_BLUE);
-		data6.setValue(attrs.get(2), VALUE_GENDER_FEMALE);
-		data6.setValue(attrs.get(3), 57);
-		training.add(data6);
-		
 		training.setClassIndex(training.numAttributes() - 1);
-		
+
 		return training;
 	}
-
 }
