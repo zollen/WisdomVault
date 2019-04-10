118809d6c1b0bb35544c165e26f7a49dbad2f83a
Stephen Kong <zollen.kong@gmail.com>
2019-04-09 20:33:58 -0400

    preparation

--- src/machinelearning/classifier/LDAClassifier3.java
+++ src/machinelearning/classifier/LDAClassifier3.java
@@ -1,45 +1,33 @@
 package machinelearning.classifier;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
-import org.ejml.data.Complex_F64;
 import org.ejml.data.DMatrixRMaj;
 import org.ejml.dense.row.CommonOps_DDRM;
-import org.ejml.simple.SimpleEVD;
-import org.ejml.simple.SimpleMatrix;
 
 import weka.core.Attribute;
-import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.converters.ArffLoader.ArffReader;
 
 public class LDAClassifier3 {
 	
 	private static final DecimalFormat ff = new DecimalFormat("0.000");
 	
-	private static final String VALUE_QUALITY_PASSED = "Y";
-	private static final String VALUE_QUALITY_FAILED = "N";
+	private static final String VALUE_FLOWER_SETOSA = "Iris-setosa";
+	private static final String VALUE_QUALITY_VERCOLOR = "Iris-versicolor";
+	private static final String VALUE_QUALITY_VIGRINICA = "Iris-virginica";
 	
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		ArrayList<String> clsVals = new ArrayList<String>();
-		clsVals.add(VALUE_QUALITY_PASSED);
-		clsVals.add(VALUE_QUALITY_FAILED);
-		
-
-		ArrayList<Attribute> attrs = new ArrayList<Attribute>();
-		Attribute attr1 = new Attribute("curvature", 1);
-		Attribute attr2 = new Attribute("diameter", 2);
-		Attribute attr3 = new Attribute("quality", clsVals, 3);
-		attrs.add(attr1);
-		attrs.add(attr2);
-		attrs.add(attr3);
-		
 		
 		// LDA Algorithm
 		// 1. Compute the mean vector for each class: Mc = (Σ xi) / ni
@@ -49,97 +37,18 @@ public class LDAClassifier3 {
 		// 5. Eigen Decomposition [V, D] = eig(inv(Sw) * Sb)
 		// 6. Get the projection matrix P is composed of the top-p eigenvectors corresponding to the largest eigenvalues
 		
-		Instances training = generateTrainingData(attrs);
-
-		List<Instance> passedList = getInstances(attr3, VALUE_QUALITY_PASSED, training);
-		List<Instance> failedList = getInstances(attr3, VALUE_QUALITY_FAILED, training);
+		Instances training = generateTrainingData("data/iris.arff.txt");
+		List<Attribute> attrs = get(training);
+		Attribute cls = training.classAttribute();
 		
-		DMatrixRMaj all = new DMatrixRMaj(convert(attr1, attr2, training));
-		DMatrixRMaj passed = new DMatrixRMaj(convert(attr1, attr2, passedList));
-		DMatrixRMaj failed = new DMatrixRMaj(convert(attr1, attr2, failedList));
+		List<Instance> setosaList = getInstances(cls, VALUE_FLOWER_SETOSA, training);
+		List<Instance> vercolorList = getInstances(cls, VALUE_QUALITY_VERCOLOR, training);
+		List<Instance> virginicaList = getInstances(cls, VALUE_QUALITY_VIGRINICA, training);
 		
-		// *** Step 1 ***
-		DMatrixRMaj passedMean = avg(attr1, attr2, passedList);
-		DMatrixRMaj failedMean = avg(attr1, attr2, failedList);
+		System.out.println(setosaList.size());
+		System.out.println(vercolorList.size());
+		System.out.println(virginicaList.size());
 		
-		// *** Step 2 ***
-		DMatrixRMaj globalMean = avg(attr1, attr2, training);
-		
-		// *** Step 3 ***
-		// Matrix_passed - [ x - avg_x_passed, y - avg_y_passed ]
-		DMatrixRMaj passed2 = meanError(passed, passedMean);
-		// Matrix_failed - [ x - avg_x_failed, y - avg_y_failed ]
-		DMatrixRMaj failed2 = meanError(failed, failedMean);
-		
-		// CoVar(passed) = (Matrix_passed * Matrix_passed') / number of passed
-		DMatrixRMaj passedWithinClasses = covariance(passed2);	
-		// CoVar(failed) = (Matrix_failed * Matrix_failed') / number of failed
-		DMatrixRMaj failedWithinClasses = covariance(failed2);
-		
-		List<DMatrixRMaj> covars1 = new ArrayList<DMatrixRMaj>();
-		covars1.add(passedWithinClasses);
-		covars1.add(failedWithinClasses);
-		
-		// CoVar(withinClasses) = CoVar(passed) * 4 / 7 + CoVar(failed) * 3 / 7
-		DMatrixRMaj covarw = covariance(training.size(), covars1);
-		
-		// inv(CoVar(withinClasses))
-		DMatrixRMaj _covarw = new DMatrixRMaj(covarw.numRows, covarw.numCols);
-		CommonOps_DDRM.invert(covarw, _covarw);
-		
-		// *** Step 4 ***
-		// Matrix_passed - [ avg_passed, avg_global ]
-		DMatrixRMaj passed3 = new DMatrixRMaj(2, 1);
-		CommonOps_DDRM.subtract(passedMean, globalMean, passed3);
-		DMatrixRMaj failed3 = new DMatrixRMaj(2, 1);
-		CommonOps_DDRM.subtract(failedMean, globalMean, failed3);
-		
-		DMatrixRMaj betweenClasses = new DMatrixRMaj(2, 2);
-		betweenClasses.set(0, 0, passed3.get(0, 0));
-		betweenClasses.set(1, 0, passed3.get(1, 0));
-		betweenClasses.set(0, 1, failed3.get(0, 0));
-		betweenClasses.set(0, 1, failed3.get(1, 0));
-		
-		// CoVar(betweenClasses) = (Matrix_passed * Matrix_passed') / number of passed
-		DMatrixRMaj covarb = covariance(betweenClasses);		
-		
-		// S = variance between classes / variance within classes
-		DMatrixRMaj S = new DMatrixRMaj(covarb.numRows, covarb.numCols);
-		
-		CommonOps_DDRM.mult(_covarw, covarb, S);
-		
-		// *** Step 5 ***
-		SimpleMatrix mat = new SimpleMatrix(S);		
-		SimpleEVD<SimpleMatrix> eigen = mat.eig();
-		
-		
-		List<Complex_F64> list = eigen.getEigenvalues();
-		System.out.println("EigenValues(inv(Sw) * Sb) = {" + ff.format(list.get(0).real) + ", " + ff.format(list.get(1).real) + "}");
-		
-		// *** Step 6 ***
-		DMatrixRMaj proj = new DMatrixRMaj(2, 2);
-		proj.set(0, 0, eigen.getEigenVector(0).get(0, 0));
-		proj.set(1, 0, eigen.getEigenVector(0).get(1, 0));
-		proj.set(0, 1, eigen.getEigenVector(1).get(0, 0));
-		proj.set(1, 1, eigen.getEigenVector(1).get(1, 0));
-		
-		DMatrixRMaj projected = new DMatrixRMaj(all.numCols, all.numRows);
-		CommonOps_DDRM.multTransB(proj, all, projected);
-		
-		System.out.println("Projected training data");
-		System.out.println(projected);
-		
-		// TEST data (x)
-		System.out.println("Testing [2.81, 5.46]...");
-		DMatrixRMaj test = new DMatrixRMaj(2, 1);
-		test.set(0, 0, 2.81);
-		test.set(1, 0, 5.46);
-		
-		DMatrixRMaj res = new DMatrixRMaj(2, 1);
-		CommonOps_DDRM.mult(proj, test, res);
-		
-		System.out.println("Projected test data");
-		System.out.println(res);
 		
 	}
 	
@@ -208,41 +117,48 @@ public class LDAClassifier3 {
 		return C;
 	}
 	
-	public static DMatrixRMaj get(Attribute attr1, Attribute attr2, Instance instance) {
+	public static DMatrixRMaj get(List<Attribute> attrs, Instance instance) {
 		
-		double x = instance.value(attr1);
-		double y = instance.value(attr2);
+		List<Double> vals = new ArrayList<Double>();
+		for (Attribute attr : attrs) {
+			vals.add(instance.value(attr));
+		}
 		
-		DMatrixRMaj data = new DMatrixRMaj(2, 1);
-		data.set(0, 0, x);
-		data.set(1, 0, y);
-		
-		return data;
-	}
-	
-	public static DMatrixRMaj avg(Attribute attr1, Attribute attr2, List<Instance> instances) {
-		
-		double x = instances.stream().mapToDouble(p -> p.value(attr1)).average().getAsDouble();
-		double y = instances.stream().mapToDouble(p -> p.value(attr2)).average().getAsDouble();
-		
-		DMatrixRMaj data = new DMatrixRMaj(2, 1);
-		data.set(0, 0, x);
-		data.set(1, 0, y);
+		DMatrixRMaj data = new DMatrixRMaj(attrs.size(), 1);
+		for (int i = 0; i < attrs.size(); i++)
+			data.set(i, 0, vals.get(i));
 		
 		return data;
 	}
 	
-	public static double [][] convert(Attribute attr1, Attribute attr2, List<Instance> instances) {
+	public static DMatrixRMaj avg(List<Attribute> attrs, List<Instance> instances) {
 		
-		double [][] data = new double[instances.size()][2];
+		List<Double> vals = new ArrayList<Double>();
+		for (Attribute attr : attrs) {
+			vals.add(instances.stream().mapToDouble(p -> p.value(attr)).average().getAsDouble());
+		}
+		
+		DMatrixRMaj data = new DMatrixRMaj(attrs.size(), 1);
+		for (int i = 0; i < attrs.size(); i++)
+			data.set(i, 0, vals.get(i));
+		
+		return data;
+	}
+	
+	public static double [][] convert(List<Attribute> attrs, List<Instance> instances) {
+		
+		double [][] data = new double[instances.size()][attrs.size()];
 		
 		AtomicInteger index = new AtomicInteger();
+
 		instances.stream().forEach(p -> {
+			
+			AtomicInteger col = new AtomicInteger();
 			
 			int idx = index.getAndIncrement();
 			
-			data[idx][0] = p.value(attr1);
-			data[idx][1] = p.value(attr2);
+			for (Attribute attr : attrs)
+				data[idx][col.getAndIncrement()] = p.value(attr);
 		});
 		
 		return data;
@@ -253,54 +169,35 @@ public class LDAClassifier3 {
 		return instances.stream().filter(p -> val.equals(p.stringValue(attr))).collect(Collectors.toList());
 	}
 	
-	public static Instances generateTrainingData(ArrayList<Attribute> attrs) {
+	public static Instances generateTrainingData(String fileName) {
 
-		Instances training = new Instances("TRAINING", attrs, 5);
+		Instances training = null;
 		
-		Instance data1 = new DenseInstance(3);	
-		data1.setValue(attrs.get(0), 2.95);
-		data1.setValue(attrs.get(1), 6.63);
-		data1.setValue(attrs.get(2), VALUE_QUALITY_PASSED);
-		training.add(data1);
+		try {
+			BufferedReader reader =
+				   new BufferedReader(new FileReader(fileName));
+			ArffReader arff = new ArffReader(reader);
+			training = arff.getData();
+			training.setClassIndex(training.numAttributes() - 1);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		Instance data2 = new DenseInstance(3);	
-		data2.setValue(attrs.get(0), 2.53);
-		data2.setValue(attrs.get(1), 7.79);
-		data2.setValue(attrs.get(2), VALUE_QUALITY_PASSED);
-		training.add(data2);
-		
-		Instance data3 = new DenseInstance(3);	
-		data3.setValue(attrs.get(0), 3.57);
-		data3.setValue(attrs.get(1), 5.65);
-		data3.setValue(attrs.get(2), VALUE_QUALITY_PASSED);
-		training.add(data3);
-		
-		Instance data4 = new DenseInstance(3);	
-		data4.setValue(attrs.get(0), 3.16);
-		data4.setValue(attrs.get(1), 5.47);
-		data4.setValue(attrs.get(2), VALUE_QUALITY_PASSED);
-		training.add(data4);
-		
-		Instance data5 = new DenseInstance(3);	
-		data5.setValue(attrs.get(0), 2.58);
-		data5.setValue(attrs.get(1), 4.46);
-		data5.setValue(attrs.get(2), VALUE_QUALITY_FAILED);
-		training.add(data5);
-		
-		Instance data6 = new DenseInstance(3);	
-		data6.setValue(attrs.get(0), 2.16);
-		data6.setValue(attrs.get(1), 6.22);
-		data6.setValue(attrs.get(2), VALUE_QUALITY_FAILED);
-		training.add(data6);
-		
-		Instance data7 = new DenseInstance(3);	
-		data7.setValue(attrs.get(0), 3.27);
-		data7.setValue(attrs.get(1), 3.52);
-		data7.setValue(attrs.get(2), VALUE_QUALITY_FAILED);
-		training.add(data7);
-		
-		training.setClassIndex(training.numAttributes() - 1);
-
 		return training;
+	}
+	
+	public static List<Attribute> get(Instances instances) {
+		
+		List<Attribute> attrs = new ArrayList<Attribute>();
+		
+		Enumeration<Attribute> list = instances.enumerateAttributes();
+		Attribute cls = instances.classAttribute();
+		
+		while (list != null && list.hasMoreElements()) {
+			attrs.add(list.nextElement());
+		}
+			
+		return attrs;
 	}
 }
