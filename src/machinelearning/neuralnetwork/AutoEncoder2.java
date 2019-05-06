844abb3f37ccd487690037df4f7a7ea4a4d45ac5
Stephen Kong <zollen.kong@gmail.com>
2019-05-06 17:29:04 -0400

    done

--- src/machinelearning/neuralnetwork/AutoEncoder2.java
+++ src/machinelearning/neuralnetwork/AutoEncoder2.java
@@ -1,23 +1,11 @@
 package machinelearning.neuralnetwork;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
-import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 
-import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
@@ -26,27 +14,43 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.math.plot.FrameView;
+import org.math.plot.Plot2DPanel;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
-import org.nd4j.linalg.dataset.SplitTestAndTrain;
-import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.learning.config.AdaDelta;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
-import org.nd4j.linalg.primitives.ImmutablePair;
+
+import com.jujutsu.tsne.PrincipalComponentAnalysis;
+
+import gui.ColoredScatterPlot;
+import weka.core.Attribute;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader.ArffReader;
 
 public class AutoEncoder2 {
-	
-	private static DecimalFormat ff = new DecimalFormat("0.000");
-	
-	private static final Random rand = new Random(0);
 	
 	// AutoEncoder vs PCA for for Dimensional reductions
 	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub	
-		System.out.println("Building model....");
+		Instances training = generateTrainingData("data/iris.arff.txt");
+		double[][] data = data(training);
+		String[] labels = labels(training);
+		
+		
+		pca(data, labels);
+		nn(data, labels);
+				
+	}
+	
+	public static void nn(double [][]data, String [] labels) {
+		
+		System.out.println("Starting Neural Network - AutoEncoder");
+		
 		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
 				.seed(0)
 				.weightInit(WeightInit.XAVIER)
@@ -55,160 +59,134 @@ public class AutoEncoder2 {
 				.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
 				.l2(0.0001)
 				.list()
-				.layer(0, new AutoEncoder.Builder().nIn(784).nOut(250).build())
-				.layer(1, new AutoEncoder.Builder().nIn(250).nOut(10).build())
-				.layer(2, new OutputLayer.Builder().nIn(10).nOut(2)
+				.layer(0, new AutoEncoder.Builder().nIn(4).nOut(2).build())
+				.layer(1, new OutputLayer.Builder().nIn(2).nOut(2)
 								.lossFunction(LossFunctions.LossFunction.MSE).build())
 				.build();
-		
 		
 		MultiLayerNetwork network = new MultiLayerNetwork(conf);
 		network.init();
 		network.setListeners(new ScoreIterationListener(1));
 		
-		System.out.println("Downloading Data...");
-		DataSetIterator itr = new MnistDataSetIterator(10000, 50000, false);
-		
-		List<INDArray> featuresTrain = new ArrayList<INDArray>();
-		List<INDArray> featuresTest = new ArrayList<INDArray>();
-		List<INDArray> labelsTest = new ArrayList<INDArray>();
-		
-		System.out.println("Preprocessing Data...");
-		while (itr.hasNext()) {
-			
-			DataSet data = itr.next();
-			
-			SplitTestAndTrain samples = data.splitTestAndTrain(80, rand);
-			
-			DataSet training = samples.getTrain();
-			DataSet testing = samples.getTest();
-			
-			featuresTrain.add(training.getFeatures());
-			featuresTest.add(testing.getFeatures());
-			
-			labelsTest.add(Nd4j.argMax(testing.getLabels(), 1));
-		}
-		
-		System.out.println("Begin Unsupervised Training...");
 		for (int nEpochs = 0; nEpochs < 30; nEpochs++) {
-			
-			for (int i = 0; i < featuresTrain.size(); i++) {
-				
-				INDArray data = featuresTrain.get(i);
-				network.fit(data, data);
-			}
-			
-			System.out.println("  --> Epoch " + nEpochs + " complete");
-		}
-		
-		
-	
-		System.out.println("Begin Testing...");
-		
-		Map<Integer, List<ImmutablePair<Double, INDArray>>> map = 
-				new HashMap<Integer, List<ImmutablePair<Double, INDArray>>>();
-		
-		for (int i = 0; i < 10; i++)
-			map.put(i, new ArrayList<ImmutablePair<Double, INDArray>>());
-		
-		for (int sample = 0; sample < featuresTest.size(); sample++) {
-			
-			INDArray tests = featuresTest.get(sample);
-			INDArray labels = labelsTest.get(sample);
-			
-			for (int row = 0; row < tests.rows(); row++) {
-				
-				INDArray test = tests.getRow(row);
-				int index = labels.getInt(row);
-				
-				double score = network.score(new DataSet(test, test));
-				
-				List<ImmutablePair<Double, INDArray>> list = map.get(index);
-				list.add(new ImmutablePair<Double, INDArray>(score, test));
-			}
-		}
-			
-		
-		
-		System.out.println("RESULT: Re-Construction Error scores...");
-		Map<String, INDArray> bests = new LinkedHashMap<String, INDArray>();
-		Map<String, INDArray> worsts = new LinkedHashMap<String, INDArray>();
-		
-		AtomicInteger count = new AtomicInteger();
-
-		map.entrySet().stream().forEach(p -> {
-			
-			count.incrementAndGet();
-			
-			p.getValue().stream().sorted(new PairComparator(1)).limit(4).forEach(
-					k -> bests.put("BEST_" + p.getKey() + "_" + ff.format(k.getKey()) + count.get(), k.getRight()));
-			p.getValue().stream().sorted(new PairComparator(-1)).limit(4).forEach(
-					k -> worsts.put("WORST_" + p.getKey() + "_" + ff.format(k.getKey()) + count.get(), k.getRight()));
-			
-			StringBuilder builder = new StringBuilder();
-			builder.append("Digit: [" + p.getKey() + "], Size: [" + p.getValue().size() + "] ==> ");
-			builder.append("Best: [" + p.getValue().stream().sorted(new PairComparator(1)).limit(4)
-							.map(k -> ff.format(k.getKey())).collect(Collectors.joining(", ")) + "]");
-			builder.append(", Worst: [" + p.getValue().stream().sorted(new PairComparator(-1)).limit(4)
-							.map(k -> ff.format(k.getKey())).collect(Collectors.joining(", ")) + "]");
-			System.out.println(builder.toString());
-		});
+			network.fit(new DataSet(convert(data), Nd4j.zeros(data.length, 2)));
+		}	
 	
 		
-		System.out.println("Creating Image Files at out directory...");		
-		bests.entrySet().stream().forEach(p -> {			
-			createImage("out/" + p.getKey() + ".png", p.getValue());
-		});
+		List<INDArray> result = network.feedForward(true);
 		
-		worsts.entrySet().stream().forEach(p -> {
-			createImage("out/" + p.getKey() + ".png", p.getValue());
-		});
+		// index 0 - input INDArray
+		// index 1 - output INDArray
+		double [][] Y = convert(result.get(1));	
+		
+		Plot2DPanel plot = new Plot2DPanel();
+		ColoredScatterPlot setosaPlot = new ColoredScatterPlot("iris", Y, labels);
+		plot.plotCanvas.setNotable(true);
+		plot.plotCanvas.setNoteCoords(true);
+		plot.plotCanvas.addPlot(setosaPlot);
 
+		FrameView plotframe = new FrameView("Neural Network - AutoEncoder", plot);
+		plotframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		plotframe.setVisible(true);
+	
 	}
 	
-	public static void createImage(String fileName, INDArray arr) {
+	public static void pca(double[][] data, String[] labels) {
 		
-		OutputStream out = null;
+		System.out.println("Starting PCA");
+
+		PrincipalComponentAnalysis pca = new PrincipalComponentAnalysis();
+		double[][] Y = pca.pca(data, 2);
 		
-		try {
-		
-			BufferedImage image = new BufferedImage(28, 28, BufferedImage.TYPE_BYTE_GRAY);
-			
-			for (int i = 0; i < 784; i++) {
-				image.getRaster().setSample(i % 28, i / 28, 0, (255 * arr.getInt(i)));
+		Plot2DPanel plot = new Plot2DPanel();
+		ColoredScatterPlot setosaPlot = new ColoredScatterPlot("iris", Y, labels);
+		plot.plotCanvas.setNotable(true);
+		plot.plotCanvas.setNoteCoords(true);
+		plot.plotCanvas.addPlot(setosaPlot);
+
+		FrameView plotframe = new FrameView("PCA", plot);
+		plotframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		plotframe.setVisible(true);
+	}
+
+	public static double[][] data(Instances instances) {
+
+		int total = instances.size();
+		int attrs = instances.numAttributes() - 1;
+
+		double[][] data = new double[total][attrs];
+
+		for (int record = 0; record < total; record++) {
+
+			Instance instance = instances.get(record);
+			double[] vals = instance.toDoubleArray();
+
+			for (int attr = 0; attr < attrs; attr++) {
+				data[record][attr] = vals[attr];
 			}
-		
-			out = new FileOutputStream(new File(fileName));
-			
-			ImageIO.write(image, "PNG", out);		
 		}
-		catch (Exception e) {
+
+		return data;
+	}
+
+	public static String[] labels(Instances instances) {
+
+		int total = instances.size();
+		Attribute cls = instances.classAttribute();
+
+		String[] data = new String[total];
+
+		for (int record = 0; record < total; record++) {
+			Instance instance = instances.get(record);
+			data[record] = instance.stringValue(cls);
+		}
+
+		return data;
+	}
+
+	public static Instances generateTrainingData(String fileName) {
+
+		Instances training = null;
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			ArffReader arff = new ArffReader(reader);
+			training = arff.getData();
+			training.setClassIndex(training.numAttributes() - 1);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		finally {
-			try {
-				if (out != null)
-					out.close();
-			}
-			catch (Exception ex) {}
-		}	
-	}
-	
-	
-	private static class PairComparator implements Comparator<ImmutablePair<Double, INDArray>> {
-		
-		private int sign = 1;
-		
-		public PairComparator(int sign) {
-			this.sign = sign;
-		}
 
-		@Override
-		public int compare(ImmutablePair<Double, INDArray> o1, ImmutablePair<Double, INDArray> o2) {
-			// TODO Auto-generated method stub
-			return o1.getKey().compareTo(o2.getKey()) * sign;
-		}
-		
+		return training;
 	}
 	
+	public static INDArray convert(double [][] data) {
+		
+		INDArray arr = Nd4j.zeros(data.length, data[0].length);
+		
+		for (int row = 0; row < data.length; row++) {
+			for (int col = 0; col < data[0].length; col++) {
+				arr.putScalar(row, col, data[row][col]);
+			}
+		}
+		
+		return arr;
+	}
+	
+	public static double [][] convert(INDArray arr) {
+		
+		double [][] data = new double[(int) arr.size(0)][(int) arr.size(1)];
+		
+		int numRecs = (int) arr.size(0);
+		int numAttrs = (int) arr.size(1);
+		
+		for (int row = 0; row < numRecs; row++) {
+			for (int col = 0; col < numAttrs; col++) {
+				data[row][col] = arr.getDouble(row, col);
+			}
+		}
+		
+		return data;
+	}
+
 }
