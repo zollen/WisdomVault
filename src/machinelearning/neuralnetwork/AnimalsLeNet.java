34ce00f6d9dae4f76834d8ff2b852552839d7156
Stephen Kong <zollen.kong@gmail.com>
2019-05-19 06:04:11 -0400

    loaded network model classifier

---
+++ src/machinelearning/neuralnetwork/AnimalsLeNet.java
@@ -0,0 +1,110 @@
+package machinelearning.neuralnetwork;
+
+import java.io.File;
+import java.util.List;
+import java.util.Random;
+
+import org.datavec.api.io.labels.PatternPathLabelGenerator;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.split.InputSplit;
+import org.datavec.image.loader.NativeImageLoader;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.nn.conf.distribution.Distribution;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+
+public class AnimalsLeNet {
+	
+	public static final int height = 100;
+	public static final int width = 100;
+	public static final int channels = 3;
+	public static final int batchSize = 320;
+	public static final int numLabels = 4;
+    
+	public static final long seed = 83;
+	public static final Random rng = new Random(seed);
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		MultiLayerNetwork network = MultiLayerNetwork.load(new File("data/lenet.model"), false);
+		
+		eval(network);
+	}
+	
+	public static ConvolutionLayer convInit(String name, int in, int out, int[] kernel, int[] stride, int[] pad, double bias) {
+        return new ConvolutionLayer.Builder(kernel, stride, pad).name(name).nIn(in).nOut(out).biasInit(bias).build();
+    }
+
+	public static ConvolutionLayer conv3x3(String name, int out, double bias) {
+        return new ConvolutionLayer.Builder(new int[]{3,3}, new int[] {1,1}, new int[] {1,1}).name(name).nOut(out).biasInit(bias).build();
+    }
+
+	public static ConvolutionLayer conv5x5(String name, int out, int[] stride, int[] pad, double bias) {
+        return new ConvolutionLayer.Builder(new int[]{5,5}, stride, pad).name(name).nOut(out).biasInit(bias).build();
+    }
+
+	public static SubsamplingLayer maxPool(String name,  int[] kernel) {
+        return new SubsamplingLayer.Builder(kernel, new int[]{2,2}).name(name).build();
+    }
+
+	public static DenseLayer fullyConnected(String name, int out, double bias, double dropOut, Distribution dist) {
+        return new DenseLayer.Builder().name(name).nOut(out).biasInit(bias).dropOut(dropOut).build();
+    }
+
+    public static RecordReaderDataSetIterator getTestData(String path) throws Exception {
+    	
+    	FileSplit fileSplit = new FileSplit(new File(path), NativeImageLoader.ALLOWED_FORMATS);
+    
+    	InputSplit [] inputSplit = fileSplit.sample(null);
+        InputSplit data = inputSplit[0];
+        
+        PatternPathLabelGenerator labelDecider = new PatternPathLabelGenerator("_", 0);
+        ImageRecordReader reader = new ImageRecordReader(height, width, channels, labelDecider);
+        reader.initialize(data);
+    	
+ 	
+    	return new RecordReaderDataSetIterator(reader, 1, 1, 4);
+    }
+    
+    public static void eval(MultiLayerNetwork network) throws Exception {
+    	
+    	// Evaluation test samples
+    	DataNormalization scaler = new ImagePreProcessingScaler(0, 1);
+    	RecordReaderDataSetIterator iter = getTestData("img/test_animals");
+        scaler.fit(iter);
+        iter.setPreProcessor(scaler);
+        
+        double total = 0.0;
+        double correct = 0.0;
+        
+        while (iter.hasNext()) {
+        	
+        	DataSet data = iter.next();
+        	ImageRecordReader reader = (ImageRecordReader) iter.getRecordReader();
+        	File file = reader.getCurrentFile();
+        	
+        	List<String> allClassLabels = iter.getLabels();
+            int labelIndex = data.getLabels().argMax(1).getInt(0);
+            int[] predictedClasses = network.predict(data.getFeatures());
+            String expectedResult = allClassLabels.get(labelIndex);
+            String modelPrediction = allClassLabels.get(predictedClasses[0]);
+            
+            if (expectedResult.equals(modelPrediction)) {
+            	correct++;
+            	System.out.println(file.getName() + " labeled [" + expectedResult + "], and the model predicted [" + modelPrediction + "]");
+            }
+            else {
+            	System.err.println(file.getName() + " labeled [" + expectedResult + "], but the model predicted [" + modelPrediction + "]");
+            }
+            total++;
+        }
+        
+        System.out.println("Accuracy: " + correct / total);
+    }
+}
