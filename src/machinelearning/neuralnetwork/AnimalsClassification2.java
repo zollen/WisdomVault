a0738eaf30544fdf514f5e9dda367fe4f4b1c2e5
Stephen Kong <zollen.kong@gmail.com>
2019-05-20 15:55:48 -0400

    work in progress

--- src/machinelearning/neuralnetwork/AnimalsClassification2.java
+++ src/machinelearning/neuralnetwork/AnimalsClassification2.java
@@ -15,14 +15,32 @@ import org.datavec.image.recordreader.ImageRecordReader;
 import org.datavec.image.transform.FlipImageTransform;
 import org.datavec.image.transform.ImageTransform;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
-import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
+import org.deeplearning4j.nn.conf.ConvolutionMode;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.WorkspaceMode;
+import org.deeplearning4j.nn.conf.graph.MergeVertex;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.ActivationLayer;
+import org.deeplearning4j.nn.conf.layers.BatchNormalization;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.api.InvocationType;
 import org.deeplearning4j.optimize.listeners.EvaluativeListener;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
 import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.nd4j.linalg.learning.config.AdaDelta;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 
 public class AnimalsClassification2 {
@@ -31,7 +49,7 @@ public class AnimalsClassification2 {
     protected static int width = 100;
     protected static int channels = 3;
     protected static int batchSize = 30;
-
+  
     protected static long seed = 83;
     protected static Random rng = new Random(seed);
     protected static int epochs = 32;
@@ -77,7 +95,7 @@ public class AnimalsClassification2 {
         // MultiLayerNetwork network = new AlexNet(height, width, channels, numLabels, 
         // seed, iterations).init();
 
-        MultiLayerNetwork network = skynetModel();
+        ComputationGraph network = skynetModel();
         network.init();
        
         /**
@@ -125,12 +143,12 @@ public class AnimalsClassification2 {
        
         if (save) {
             System.out.println("Save model....");
-            network.save(new File("data/lenet.model"));
+            network.save(new File("data/skynet.model"));
         }
         System.out.println("****************Example finished********************");
     }
     
-    private void eval(MultiLayerNetwork network) throws Exception {
+    private void eval(ComputationGraph network) throws Exception {
     	
     	// Evaluation test samples
     	DataNormalization scaler = new ImagePreProcessingScaler(0, 1);
@@ -149,9 +167,9 @@ public class AnimalsClassification2 {
         	
         	List<String> allClassLabels = iter.getLabels();
             int labelIndex = data.getLabels().argMax(1).getInt(0);
-            int[] predictedClasses = network.predict(data.getFeatures());
+            INDArray [] predictedClasses = network.output(false, data.getFeatures());
             String expectedResult = allClassLabels.get(labelIndex);
-            String modelPrediction = allClassLabels.get(predictedClasses[0]);
+            String modelPrediction = allClassLabels.get(predictedClasses[0].getInt(0));
             
             if (expectedResult.equals(modelPrediction)) {
             	correct++;
@@ -167,11 +185,151 @@ public class AnimalsClassification2 {
     }
 
    
-    public MultiLayerNetwork skynetModel() {
+    public ComputationGraph skynetModel() {
+    	
+		ComputationGraphConfiguration.GraphBuilder graph = new NeuralNetConfiguration.Builder()
+				.seed(seed).l2(0.005)
+				.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+				.activation(Activation.RELU)
+				.weightInit(WeightInit.XAVIER)
+				.updater(new AdaDelta())
+				.convolutionMode(ConvolutionMode.Same)
+				.inferenceWorkspaceMode(WorkspaceMode.ENABLED)
+				.trainingWorkspaceMode(WorkspaceMode.ENABLED)
+				.graphBuilder();
+
+		ComputationGraphConfiguration conf = graph
+			.addInputs("inputs1").setInputTypes(InputType.convolutional(height, width, channels))
+			
+			.addLayer("incept-1-1", convolution("1x1c", channels, 1, new int[] { 1, 1 }, new int[] { 1, 1 }), 
+					"inputs1")
+			.addLayer("incept-1-2", batchNormalization("batch1"), 
+					"incept-1-1")
+			.addLayer("incept-1-3", activation("activation1", Activation.RELU), 
+					"incept-1-2")
+		
+			.addLayer("incept-2-1", convolution("1x1c", channels, 1, new int[] { 1, 1 }, new int[] { 1, 1 }), 
+					"inputs1")
+			.addLayer("incept-2-2", convolution("3x3c", 50, 50, new int[] { 3, 3 }, new int[] { 1, 1 }, new int [] { 0, 0 }), 
+					"incept-2-1")	
+			.addLayer("incept-2-3a", convolution("3x1c", 50, 50, new int[] { 3, 1 }, new int[] { 1, 1 }, new int [] { 0, 0 }), 
+					"incept-2-2")
+			.addLayer("incept-2-3b", convolution("1x3c", 50, 50, new int[] { 1, 3 }, new int[] { 1, 1 }, new int [] { 0, 0 }), 
+					"incept-2-2")		
+			.addVertex("vertex-2-4", new MergeVertex(), 
+					"incept-2-3a", "incept-2-3b")
+			.addLayer("incept-2-5", batchNormalization("batch1"), 
+					"vertex-2-4")
+			.addLayer("incept-2-6", activation("activation1", Activation.RELU), 
+					"incept-2-5")
+						
+			.addLayer("incept-3-1", convolution("1x1c", channels, 1, new int[] { 1, 1 }, new int[] { 1, 1 }),
+					"inputs1")
+			.addLayer("incept-3-2a", convolution("3x1c", 50, 50, new int[] { 3, 1 }, new int[] { 1, 1 }), 
+					"incept-3-1")
+			.addLayer("incept-3-2b", convolution("1x3c", 50, 50, new int[] { 1, 3 }, new int[] { 1, 1 }), 
+					"incept-3-1")			
+			.addVertex("vertex-3-3", new MergeVertex(), 
+					"incept-3-2a", "incept-3-2b")
+			.addLayer("incept-3-4", batchNormalization("batch1"), 
+					"vertex-3-3")
+			.addLayer("incept-3-5", activation("activation1", Activation.RELU), 
+					"incept-3-4")
+		
+			.addVertex("incept-123", new MergeVertex(),
+					"incept-1-3", "incept-2-6", "incept-3-5")
+			
+			.addLayer("incept-4-1", maxPooling("maxpool4", new int[] { 2, 2 }), 
+					"incept-123")
+		
+			.addLayer("incept-5", new DenseLayer.Builder().nIn(500).nOut(500).dropOut(0.5).build(), 
+					"incept-4-1")
+			
+			.addLayer("incept-6", new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+					.nIn(500)
+					.nOut(numLabels)
+                .activation(Activation.SOFTMAX).build(), 
+                "incept-5")
+			
+			.setOutputs("incept-6")
+            .build();
+		
+		
+
+        return new ComputationGraph(conf);
+    }
+    
+    private static SubsamplingLayer maxPooling(String name, int [] ... args) {
+    	
+    	int [] kernel = null;
+    	int [] stride = null;
+    	int [] pad = null;
+    	
+    	int index = 0;
+    	for (int [] arg : args) {
+    		switch(index) {
+    		case 0:
+    			kernel = arg;
+    		break;
+    		case 1:
+    			stride = arg;
+    		break;
+    		default:
+    			pad = arg;
+    		}
+    		index++;
+    	}
+    	
+    	if (stride == null) {
+    		stride = new int[] { 1, 1 };
+    	}
+    	
+    	if (pad == null) {
+    		pad = new int[] { 0, 0 };
+    	}
+    	
+        return new SubsamplingLayer.Builder(kernel, stride, pad).name(name).build();
+    }
+    
+    public static ActivationLayer activation(String name, Activation algo) {
+    	return new ActivationLayer.Builder().name(name).activation(algo).dropOut(0.2).build();
+    }
+    
+    public static BatchNormalization batchNormalization(String name) {
+    	return new BatchNormalization.Builder(false).name(name).build();
+    }
+    
+    public static ConvolutionLayer convolution(String name, int in, int out, int [] ... args) {
     	
     	
-      
-        return null;
+    	int [] kernel = null;
+    	int [] stride = null;
+    	int [] pad = null;
+    	
+    	int index = 0;
+    	for (int [] arg : args) {
+    		switch(index) {
+    		case 0:
+    			kernel = arg;
+    		break;
+    		case 1:
+    			stride = arg;
+    		break;
+    		default:
+    			pad = arg;
+    		}
+    		index++;
+    	}
+    	
+    	if (stride == null) {
+    		stride = new int[] { 1, 1 };
+    	}
+    	
+    	if (pad == null) {
+    		pad = new int[] { 0, 0 };
+    	}
+    	
+    	return new ConvolutionLayer.Builder(kernel, stride, pad).name(name).nIn(in).nOut(out).build();   	
     }
 
     public DataSetIterator getIterator(ImageRecordReader trainRR, InputSplit trainData) throws Exception {
