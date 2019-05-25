1fcd07091d1c62661c2acc1225e043a6e32ad3cd
Stephen Kong <zollen.kong@gmail.com>
2019-05-24 21:39:45 -0400

    need MultiDataSetIteratorsIterator

--- src/machinelearning/neuralnetwork/AnimalsClassification2.java
+++ src/machinelearning/neuralnetwork/AnimalsClassification2.java
@@ -3,7 +3,9 @@ package machinelearning.neuralnetwork;
 
 import java.io.File;
 import java.util.List;
+import java.util.Properties;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import org.datavec.api.io.filters.BalancedPathFilter;
 import org.datavec.api.io.labels.ParentPathLabelGenerator;
@@ -15,8 +17,37 @@ import org.datavec.image.loader.NativeImageLoader;
 import org.datavec.image.recordreader.ImageRecordReader;
 import org.datavec.image.transform.FlipImageTransform;
 import org.datavec.image.transform.ImageTransform;
+import org.deeplearning4j.api.storage.StatsStorage;
+import org.deeplearning4j.arbiter.ComputationGraphSpace;
+import org.deeplearning4j.arbiter.conf.updater.NadamSpace;
+import org.deeplearning4j.arbiter.layers.BatchNormalizationSpace;
+import org.deeplearning4j.arbiter.layers.DenseLayerSpace;
+import org.deeplearning4j.arbiter.layers.OutputLayerSpace;
+import org.deeplearning4j.arbiter.layers.SeparableConvolution2DLayerSpace;
+import org.deeplearning4j.arbiter.layers.SubsamplingLayerSpace;
+import org.deeplearning4j.arbiter.optimize.api.CandidateGenerator;
+import org.deeplearning4j.arbiter.optimize.api.OptimizationResult;
+import org.deeplearning4j.arbiter.optimize.api.ParameterSpace;
+import org.deeplearning4j.arbiter.optimize.api.data.DataSource;
+import org.deeplearning4j.arbiter.optimize.api.saving.ResultReference;
+import org.deeplearning4j.arbiter.optimize.api.saving.ResultSaver;
+import org.deeplearning4j.arbiter.optimize.api.score.ScoreFunction;
+import org.deeplearning4j.arbiter.optimize.api.termination.MaxCandidatesCondition;
+import org.deeplearning4j.arbiter.optimize.api.termination.MaxTimeCondition;
+import org.deeplearning4j.arbiter.optimize.api.termination.TerminationCondition;
+import org.deeplearning4j.arbiter.optimize.config.OptimizationConfiguration;
+import org.deeplearning4j.arbiter.optimize.generator.RandomSearchGenerator;
+import org.deeplearning4j.arbiter.optimize.parameter.continuous.ContinuousParameterSpace;
+import org.deeplearning4j.arbiter.optimize.parameter.integer.IntegerParameterSpace;
+import org.deeplearning4j.arbiter.optimize.runner.IOptimizationRunner;
+import org.deeplearning4j.arbiter.optimize.runner.LocalOptimizationRunner;
+import org.deeplearning4j.arbiter.saver.local.FileModelSaver;
+import org.deeplearning4j.arbiter.scoring.impl.EvaluationScoreFunction;
+import org.deeplearning4j.arbiter.task.ComputationGraphTaskCreator;
+import org.deeplearning4j.arbiter.ui.listener.ArbiterStatusListener;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.CacheMode;
 import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
 import org.deeplearning4j.nn.conf.ConvolutionMode;
 import org.deeplearning4j.nn.conf.GradientNormalization;
@@ -30,10 +61,15 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.conf.layers.SeparableConvolution2D;
 import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
 import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.api.InvocationType;
 import org.deeplearning4j.optimize.listeners.EvaluativeListener;
+import org.deeplearning4j.optimize.listeners.PerformanceListener;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.ui.storage.FileStatsStorage;
+import org.deeplearning4j.ui.weights.ConvolutionalIterationListener;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.api.ops.impl.indexaccum.IAMax;
@@ -65,40 +101,104 @@ public class AnimalsClassification2 {
 
 
 	public void run(String[] args) throws Exception {
-
-		System.out.println("Build model....");
-
-		ComputationGraph network = playModel();
-		network.init();
 		
-		
-		// test iterator
-		DataSetIterator testIter = getIterator("img/test_animals", new PatternPathLabelGenerator("_", 0));
+		if (args.length > 0 && args[0].indexOf("O") >= 0) {
 
+			System.out.println("Optimize model....");
 
-		// listeners
-		network.setListeners(new ScoreIterationListener(10),
-				new EvaluativeListener(testIter, 1, InvocationType.EPOCH_END));
+			ComputationGraphSpace space = optimizeModel();
 
-		System.out.println(network.summary());
+			CandidateGenerator candidateGenerator = new RandomSearchGenerator(space, null);
+			Class<? extends DataSource> dataSourceClass = AnimalsDataSource.class;
 
-		// Train without transformations
-		DataSetIterator trainIter1 = getIterator("img/animals", new ParentPathLabelGenerator());
-		DataSetIterator trainIter2 = getIterator("img/animals", new ParentPathLabelGenerator(), new FlipImageTransform(1));
-		
+			@SuppressWarnings("deprecation")
+			ScoreFunction scoreFunction = new EvaluationScoreFunction(
+					org.deeplearning4j.eval.Evaluation.Metric.ACCURACY);
 
-		System.out.println("Train model....");
-		network.fit(trainIter1, epochs);
-		network.fit(trainIter2, epochs);
+			TerminationCondition[] terminationConditions = { new MaxTimeCondition(15, TimeUnit.MINUTES),
+					new MaxCandidatesCondition(10) };
 
-		// Evaluation test samples
-		eval(network);
+			ResultSaver modelSaver = new FileModelSaver("out");
+			OptimizationConfiguration configuration = new OptimizationConfiguration.Builder()
+					.candidateGenerator(candidateGenerator)
+					.dataSource(dataSourceClass, new Properties())
+					.modelSaver(modelSaver)
+					.scoreFunction(scoreFunction)
+					.terminationConditions(terminationConditions)
+					.build();
 
-		if (save) {
-			System.out.println("Save model....");
-			network.save(new File("data/playnet.model"));
+			IOptimizationRunner runner = new LocalOptimizationRunner(configuration, new ComputationGraphTaskCreator());
+
+			StatsStorage ss = new FileStatsStorage(new File("out/animalsStats.dl4j"));
+			runner.addListeners(new ArbiterStatusListener(ss));
+			UIServer.getInstance().attach(ss);
+
+			runner.execute();
+
+			String s = "Best score: " + runner.bestScore() + "\n" + "Index of model with best score: "
+					+ runner.bestScoreCandidateIndex() + "\n" + "Number of configurations evaluated: "
+					+ runner.numCandidatesCompleted() + "\n";
+			System.out.println(s);
+
+			int indexOfBestResult = runner.bestScoreCandidateIndex();
+			List<ResultReference> allResults = runner.getResults();
+
+			OptimizationResult bestResult = allResults.get(indexOfBestResult).getResult();
+			MultiLayerNetwork bestModel = (MultiLayerNetwork) bestResult.getResultReference().getResultModel();
+
+			System.out.println("\n\nConfiguration of best model:\n");
+			System.out.println(bestModel.getLayerWiseConfigurations().toJson());
+
+			UIServer.getInstance().stop();
+			
+		} else {
+
+			System.out.println("Build model....");
+
+			ComputationGraph network = playModel();
+			network.init();
+
+			// test iterator
+			DataSetIterator testIter = getIterator("img/test_animals", new PatternPathLabelGenerator("_", 0));
+
+			// listeners
+			network.setListeners(new ScoreIterationListener(10), 
+					new PerformanceListener(10, false),
+					new ConvolutionalIterationListener(10, true),
+					new EvaluativeListener(testIter, 1, InvocationType.EPOCH_END));
+
+			System.out.println(network.summary());
+
+			// Train without transformations
+			DataSetIterator trainIter1 = getIterator("img/animals", new ParentPathLabelGenerator());
+			DataSetIterator trainIter2 = getIterator("img/animals", new ParentPathLabelGenerator(),
+					new FlipImageTransform(1));
+
+			/*
+			 * List<Pair<ImageTransform,Double>> pipeline = Arrays.asList( new Pair<>(new
+			 * FlipImageTransform(1), 0.6), new Pair<>(new ScaleImageTransform(rng, 30),
+			 * 0.7), new Pair<>(new CropImageTransform(rng, 20), 0.7), new Pair<>(new
+			 * EqualizeHistTransform(rng, 39), 0.7), new Pair<>(new
+			 * ColorConversionTransform(rng, 1), 0.7), new Pair<>(new
+			 * WarpImageTransform(rng, 70), 0.7));
+			 * 
+			 * DataSetIterator trainIter3 = getIterator("img/animals", new
+			 * ParentPathLabelGenerator(), new PipelineImageTransform(pipeline, true));
+			 */
+
+			System.out.println("Train model....");
+			network.fit(trainIter1, epochs);
+			network.fit(trainIter2, epochs);
+
+			// Evaluation test samples
+			eval(network);
+
+			if (save) {
+				System.out.println("Save model....");
+				network.save(new File("data/playnet.model"));
+			}
+			System.out.println("****************Example finished********************");
 		}
-		System.out.println("****************Example finished********************");
 	}
     
     private void eval(ComputationGraph network) throws Exception {
@@ -139,20 +239,85 @@ public class AnimalsClassification2 {
         
         System.out.println("Accuracy: " + correct / total);
     }
-
+    
+    public ComputationGraphSpace optimizeModel() {
+    	
+    	ParameterSpace<Double> learningRateHyperparam = new ContinuousParameterSpace(0.0001, 0.1);  
+		ParameterSpace<Double> beta1Hyperparam = new ContinuousParameterSpace(0.0, 0.999); 
+		ParameterSpace<Double> beta2Hyperparam = new ContinuousParameterSpace(0.0, 0.999); 
+		ParameterSpace<Double> epsilonHyperparam = new ContinuousParameterSpace(0.0, 0.999); 
+		ParameterSpace<Double> l2Hyperparam = new ContinuousParameterSpace(0.0001, 0.09);
+        ParameterSpace<Integer> layer1BandwidthHyperparam = new IntegerParameterSpace(16, 1024); 
+        ParameterSpace<Integer> layer4BandwidthHyperparam = new IntegerParameterSpace(16, 1024); 
+        ParameterSpace<Integer> layer7BandwidthHyperparam = new IntegerParameterSpace(16, 1024);
+        ParameterSpace<Integer> layer10BandwidthHyperparam = new IntegerParameterSpace(16, 1024);
+        
+        return new ComputationGraphSpace.Builder() 
+			.seed(seed)
+			.l2(l2Hyperparam)
+			.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+			.weightInit(WeightInit.RELU)
+			.activation(Activation.RELU)
+			.updater(new NadamSpace(learningRateHyperparam, beta1Hyperparam, beta2Hyperparam, epsilonHyperparam))
+			.gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
+			.convolutionMode(ConvolutionMode.Same)
+			.inferenceWorkspaceMode(WorkspaceMode.ENABLED)
+			.trainingWorkspaceMode(WorkspaceMode.ENABLED)
+			.numEpochs(epochs)
+			
+			.addInputs("inputs1").setInputTypes(InputType.convolutional(height, width, channels))
+			
+			.addLayer("1.1-5x5", convolspace(channels, layer1BandwidthHyperparam, new int[] { 5, 5 }), 
+					"inputs1")	
+			.addLayer("1.2-batch", batchspace(), 
+					"1.1-5x5")
+			.addLayer("1.3-maxpool", poolspace(new int[] { 2, 2 }, new int[] { 2, 2 }), 
+					"1.2-batch")
+			
+			.addLayer("1.4-3x3", convolspace(layer1BandwidthHyperparam, layer4BandwidthHyperparam, new int[] { 3, 3 }),
+					"1.3-maxpool")
+			.addLayer("1.5-batch", batchspace(), 
+					"1.4-3x3")
+			.addLayer("1.6-maxpool", poolspace(new int[] { 2, 2 }, new int[] { 2, 2 }),
+					"1.5-batch")
+			
+			.addLayer("1.7-3x3", convolspace(layer4BandwidthHyperparam, layer7BandwidthHyperparam, new int[] { 3, 3 }),
+					"1.6-maxpool")
+			.addLayer("1.8-batch", batchspace(), 
+					"1.7-3x3")
+			.addLayer("1.9-maxpool", poolspace(new int[] { 2, 2 }, new int[] { 2, 2 }),
+					"1.8-batch")
+	
+			.addLayer("1.10-dense", new DenseLayerSpace.Builder()
+					.nOut(layer10BandwidthHyperparam)
+					.dropOut(0.5).build(), 
+					"1.9-maxpool")
+			.addLayer("1.11-output", new OutputLayerSpace.Builder()
+					.lossFunction(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+					.nIn(layer10BandwidthHyperparam)
+					.nOut(numLabels)
+	                .activation(Activation.SOFTMAX)
+					.build(), 
+					"1.10-dense")
+			
+			.setOutputs("1.11-output")
+            .build();
+		
+    }
     
     public ComputationGraph playModel() {
     	
     	ComputationGraphConfiguration.GraphBuilder graph = new NeuralNetConfiguration.Builder()
 				.seed(seed)
 				.l2(0.005)
+				.cacheMode(CacheMode.HOST)
 				.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
 				.weightInit(WeightInit.RELU)
 				.activation(Activation.RELU)
 				.updater(new AdaDelta())
 				.gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
 				.convolutionMode(ConvolutionMode.Same)
-				.cudnnAlgoMode(ConvolutionLayer.AlgoMode.NO_WORKSPACE)
+				.cudnnAlgoMode(ConvolutionLayer.AlgoMode.PREFER_FASTEST)
 				.inferenceWorkspaceMode(WorkspaceMode.ENABLED)
 				.trainingWorkspaceMode(WorkspaceMode.ENABLED)
 				.graphBuilder();
@@ -198,7 +363,7 @@ public class AnimalsClassification2 {
 		return new ComputationGraph(conf);
     }
    
-    private static SubsamplingLayer maxPooling(String name, int [] ... args) {
+    public static SubsamplingLayer maxPooling(String name, int [] ... args) {
     	
     	int [] kernel = null;
     	int [] stride = null;
@@ -229,9 +394,45 @@ public class AnimalsClassification2 {
     	
         return new SubsamplingLayer.Builder(kernel, stride, pad).name(name).build();
     }
+    
+    public static SubsamplingLayerSpace poolspace(int [] ... args) {
+    	
+    	int [] kernel = null;
+    	int [] stride = null;
+    	int [] pad = null;
+    	
+    	int index = 0;
+    	for (int [] arg : args) {
+    		switch(index) {
+    		case 0:
+    			kernel = arg;
+    		break;
+    		case 1:
+    			stride = arg;
+    		break;
+    		default:
+    			pad = arg;
+    		}
+    		index++;
+    	}
+    	
+    	if (stride == null) {
+    		stride = new int[] { 1, 1 };
+    	}
+    	
+    	if (pad == null) {
+    		pad = new int[] { 0, 0 };
+    	}
+    	
+    	return new SubsamplingLayerSpace.Builder().kernelSize(kernel).stride(stride).padding(pad).build();
+    }
   
     public static BatchNormalization batchNormalization(String name) {
     	return new BatchNormalization.Builder(false).name(name).build();
+    }
+    
+    public static BatchNormalizationSpace batchspace() {
+    	return new BatchNormalizationSpace.Builder().lockGammaBeta(false).build();
     }
     
     public static ConvolutionLayer convolution(String name, int in, int out, int [] ... args) {
@@ -239,8 +440,7 @@ public class AnimalsClassification2 {
     }
         
     public static ConvolutionLayer convolution(String name, int in, int out, Activation activation, int [] ... args) {
-    	
-    	
+    	   	
     	int [] kernel = null;
     	int [] stride = null;
     	int [] pad = null;
@@ -271,11 +471,73 @@ public class AnimalsClassification2 {
     	return new SeparableConvolution2D.Builder(kernel, stride, pad).name(name).nIn(in).nOut(out).depthMultiplier(2).activation(activation).build();   	
     }
     
-    private DataSetIterator getIterator(String pathName, PathLabelGenerator labelMaker) throws Exception {
+    public static SeparableConvolution2DLayerSpace convolspace(int in, int out, int [] ... args) {
+    	return convolspace(in, out, null, null, Activation.RELU, args);
+    }
+    
+    public static SeparableConvolution2DLayerSpace convolspace(int in, ParameterSpace<Integer> out, int [] ... args) {
+    	return convolspace(in, null, null, out, Activation.RELU, args);
+    }
+    
+    public static SeparableConvolution2DLayerSpace convolspace(ParameterSpace<Integer> in, int out, int [] ... args) {
+    	return convolspace(null, out, in, null, Activation.RELU, args);
+    }
+    
+    public static SeparableConvolution2DLayerSpace convolspace(ParameterSpace<Integer> in, ParameterSpace<Integer> out, int [] ... args) {
+    	return convolspace(null, null, in, out, Activation.RELU, args);
+    }
+    
+    public static SeparableConvolution2DLayerSpace convolspace(Integer in, Integer out, ParameterSpace<Integer> ins, ParameterSpace<Integer> outs, Activation activation, int [] ... args) {
+    	
+    	int [] kernel = null;
+    	int [] stride = null;
+    	int [] pad = null;
+    	
+    	int index = 0;
+    	for (int [] arg : args) {
+    		switch(index) {
+    		case 0:
+    			kernel = arg;
+    		break;
+    		case 1:
+    			stride = arg;
+    		break;
+    		default:
+    			pad = arg;
+    		}
+    		
+    		index++;
+    	}
+    	
+    	if (stride == null) {
+    		stride = new int[] { 1, 1 };
+    	}
+    	
+    	if (pad == null) {
+    		pad = new int[] { 0, 0 };
+    	}
+    	
+    	if (in != null && out != null && ins == null && outs == null) {
+    		return new SeparableConvolution2DLayerSpace.Builder().nIn(in).nOut(out).kernelSize(kernel).stride(stride).padding(pad).depthMultiplier(2).activation(activation).build();   
+    	}
+    	else
+    	if (in != null && out == null && ins == null && outs != null) {   		
+    		return new SeparableConvolution2DLayerSpace.Builder().nIn(in).nOut(outs).kernelSize(kernel).stride(stride).padding(pad).depthMultiplier(2).activation(activation).build();  
+    	}
+    	else
+    	if (in == null && out != null && ins != null && outs == null) {   		
+    		return new SeparableConvolution2DLayerSpace.Builder().nIn(ins).nOut(out).kernelSize(kernel).stride(stride).padding(pad).depthMultiplier(2).activation(activation).build();
+    	}
+    	else {
+    		return new SeparableConvolution2DLayerSpace.Builder().nIn(ins).nOut(outs).kernelSize(kernel).stride(stride).padding(pad).depthMultiplier(2).activation(activation).build(); 
+    	}
+    }
+    
+    private static DataSetIterator getIterator(String pathName, PathLabelGenerator labelMaker) throws Exception {
     	return getIterator(pathName, labelMaker, null);
     }
     
-    private DataSetIterator getIterator(String pathName, PathLabelGenerator labelMaker, ImageTransform transform) throws Exception {
+    private static DataSetIterator getIterator(String pathName, PathLabelGenerator labelMaker, ImageTransform transform) throws Exception {
 
 		FileSplit fileSplit = new FileSplit(new File(pathName), NativeImageLoader.ALLOWED_FORMATS, rng);
 		int numExamples = Math.toIntExact(fileSplit.length());
@@ -317,4 +579,49 @@ public class AnimalsClassification2 {
     public static void main(String[] args) throws Exception {
         new AnimalsClassification2().run(args);
     }
+    
+    
+    public static class AnimalsDataSource implements DataSource {
+		
+		private static final long serialVersionUID = 1L;
+		
+		private DataSetIterator training = null;
+		private DataSetIterator testing = null;
+		
+		public AnimalsDataSource() throws Exception {
+			
+			DataSetIterator trainIter1 = getIterator("img/animals", new ParentPathLabelGenerator());
+			DataSetIterator trainIter2 = getIterator("img/animals", new ParentPathLabelGenerator(), new FlipImageTransform(1));
+			DataSetIterator testIter1 = getIterator("img/test_animals", new PatternPathLabelGenerator("_", 0));
+					
+			this.training = trainIter1;
+			this.testing = testIter1;
+		}
+		
+		@Override
+		public void configure(Properties properties) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public Class<?> getDataType() {
+			// TODO Auto-generated method stub
+			return DataSetIterator.class;
+		}
+
+		@Override
+		public Object testData() {
+			// TODO Auto-generated method stub
+			return testing;
+		}
+
+		@Override
+		public Object trainData() {
+			// TODO Auto-generated method stub
+			return training;
+		}
+		 
+		 
+	}
+
 }
