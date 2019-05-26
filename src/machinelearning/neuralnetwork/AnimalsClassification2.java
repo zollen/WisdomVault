29fd41e3cfc1417a51f2568cab0b6031bb6fdd10
Stephen Kong <zollen.kong@gmail.com>
2019-05-26 07:50:11 -0400

    increase max time

--- src/machinelearning/neuralnetwork/AnimalsClassification2.java
+++ src/machinelearning/neuralnetwork/AnimalsClassification2.java
@@ -129,7 +129,7 @@ public class AnimalsClassification2 {
 					org.deeplearning4j.eval.Evaluation.Metric.ACCURACY);
 
 			TerminationCondition[] terminationConditions = { 
-					new MaxTimeCondition(48, TimeUnit.HOURS),
+					new MaxTimeCondition(96, TimeUnit.HOURS),
 					new MaxCandidatesCondition(6)
 			};
 
