ed7e3e32ed6ab6c77781ab9a23e2f3470d6bcf44
Stephen Kong <zollen.kong@gmail.com>
2019-06-10 19:44:36 -0400

    work in progress

--- src/machinelearning/neuralnetwork/autoencoder/AutoEncoder4.java
+++ src/machinelearning/neuralnetwork/autoencoder/AutoEncoder4.java
@@ -2,8 +2,6 @@ package machinelearning.neuralnetwork.autoencoder;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
@@ -13,7 +11,6 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
-import org.nd4j.evaluation.classification.Evaluation;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
@@ -21,7 +18,6 @@ import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.learning.config.Nadam;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
-import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader.ArffReader;
 
@@ -29,13 +25,11 @@ public class AutoEncoder4 {
 	
 	public static void main(String [] args) throws Exception {
 		
-        DataSet data = generateTrainingData("data/iris.arff.txt");
-	//	DataSet data = getData("data/iris.arff.txt");
-        
-   
-        System.out.println("NUM OF INPUTS: " + data.numInputs());
-        
-               
+		DataSet data = generateData("data/iris.arff.txt");
+		
+		INDArray expected = data.getFeatures();
+		
+		
         System.out.println("Building model....");
 		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
 				.seed(0)
@@ -59,17 +53,23 @@ public class AutoEncoder4 {
 		
 		System.out.println(network.summary());
 		
-		for (int i = 0; i < 30; i++)
+		for (int i = 0; i < 30; i++) {
 			network.fit(data);
+		}
+
+		INDArray actual = network.output(expected);
 		
-		Evaluation eval = new Evaluation(4);
-		INDArray output = network.output(data.getFeatures());	
-		eval.eval(data.getLabels(), output);
-		System.out.println(eval.stats());   
+		if (actual.equalsWithEps(expected, 0.001))
+			System.out.println("INPUTS AND OUTPUTS ARE IDENTIAL!!!");
+		else
+			System.err.println("INPUTS AND OUTPUTS ARE NOT THE SAME!!!");	
+		
+		System.out.println(expected.getRow(0));
+		System.out.println(actual.getRow(0));
 	}
 	
-	public static DataSet generateTrainingData(String fileName) {
-
+	public static DataSet generateData(String fileName) {
+	
 		try {
 			
 			BufferedReader reader = new BufferedReader(new FileReader(fileName));
@@ -92,47 +92,5 @@ public class AutoEncoder4 {
 		}
 
 		return null;
-	}
-	
-	private static DataSet getData(String fileName) throws Exception {
-		
-		BufferedReader reader = null;
-		
-		Map<String,Integer> map = new HashMap<String, Integer>();
-		map.put("Iris-setosa", 0);
-		map.put("Iris-versicolor", 1);
-		map.put("Iris-virginica", 2);
-		
-		try {
-			reader = new BufferedReader(new FileReader(fileName));
-			ArffReader arff = new ArffReader(reader);
-			Instances instances = arff.getData();
-			instances.setClassIndex(instances.numAttributes() - 1);
-			
-			INDArray inputs = Nd4j.zeros(instances.numInstances(), instances.numAttributes() - 1);
-			INDArray labels = Nd4j.zeros(instances.numInstances(), 3);
-
-			for (int row = 0; row < instances.numInstances(); row++) {
-				
-				Instance instance = instances.get(row);
-				
-				for (int col = 0; col < instances.numAttributes() - 1; col++) {
-					inputs.putScalar(row, col, instance.value(col));
-				}
-				
-				int position = map.get(instance.stringValue(instances.numAttributes() - 1));
-				
-				labels.putScalar(row, position, 1);
-			}
-			
-			return new DataSet(inputs, labels);
-		} 
-		finally {
-			try {
-				if (reader != null)
-					reader.close();
-			}
-			catch (Exception ex) {}
-		}
 	}
 }
