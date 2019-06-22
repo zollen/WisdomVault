2ef7de1b5306153cfd2e25e28d9f86fd0f009973
Stephen Kong <zollen.kong@gmail.com>
2019-06-22 06:10:29 -0400

    support  VAE

--- src/machinelearning/neuralnetwork/autoencoder/AutoEncoder4.java
+++ src/machinelearning/neuralnetwork/autoencoder/AutoEncoder4.java
@@ -6,6 +6,8 @@ import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.AutoEncoder;
 import org.deeplearning4j.nn.conf.layers.CenterLossOutputLayer;
+import org.deeplearning4j.nn.conf.layers.variational.GaussianReconstructionDistribution;
+import org.deeplearning4j.nn.conf.layers.variational.VariationalAutoencoder;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
@@ -36,9 +38,40 @@ public class AutoEncoder4 {
 		preprocessor.fit(data);
 		preprocessor.preProcess(data);
 		
-        System.out.println("Building model....");
+		MultiLayerNetwork ae = ae();
+		MultiLayerNetwork vae = vae();
+		
+		ae.init();
+		vae.init();
+		
+		ae.setListeners(new ScoreIterationListener(1000));
+		vae.setListeners(new ScoreIterationListener(1000));
+		
+		System.out.println(ae.summary());
+		System.out.println(vae.summary());
+		
+		System.out.println("Let's overfitting both networks!!!!!!!!!!!!");
+		for (int i = 0; i < 10000; i++) {
+			ae.fit(expected, expected);
+			vae.fit(expected, expected);
+		}
+
+		INDArray actual_ae = ae.output(expected);
+		INDArray actual_vae = vae.output(expected);
+		
+		System.out.println("=========== EXPECTED ============");
+		System.out.println(expected);
+		System.out.println("============ AE OUTPUT ===========");
+		System.out.println(actual_ae);
+		System.out.println("=========== VAE OUTPUT ===========");
+		System.out.println(actual_vae);
+	}
+	
+	public static MultiLayerNetwork ae() {
+		
+		System.out.println("Building AE....");
 		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-				.seed(0)
+				.seed(83)
 				.cacheMode(CacheMode.HOST)
 				.weightInit(WeightInit.XAVIER)
 				.updater(new Nadam())
@@ -60,23 +93,63 @@ public class AutoEncoder4 {
 						.lossFunction(LossFunctions.LossFunction.XENT).build())
 				.build();
 		
-		MultiLayerNetwork network = new MultiLayerNetwork(conf);
-		network.init();
+		return new MultiLayerNetwork(conf);
+	}
+	
+	public static MultiLayerNetwork vae() {
 		
+		/**
+		 * In the variational autoencoder, pp is specified as a standard Normal distribution 
+		 * with mean zero and variance one, or p(z) = Normal(0,1)p(z)=Normal(0,1). If the 
+		 * encoder outputs representations z (hidden layer) that are different than those from a 
+		 * standard normal distribution, it will receive a penalty in the loss. This regularizer 
+		 * term means ‘keep the representations z (hidden layer) of each digit sufficiently diverse’. 
+		 * If we didn’t include the regularizer, the encoder could learn to cheat and give each 
+		 * datapoint a representation in a different region of Euclidean space. This is bad, 
+		 * because then two images of the same number (say a 2 written by different people, 
+		 * 2 by {alice} and 2 by {bob}) could end up with very different representations 
+		 * z_{alice}, z_{bob}. We want the representation space of z to be meaningful, so we 
+		 * penalize this behavior. This has the effect of keeping similar inputs representations 
+		 * close together. 
+		 */
 		
-		network.setListeners(new ScoreIterationListener(1));
+		System.out.println("Building VAE....");
+		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+				.seed(83)
+				.cacheMode(CacheMode.HOST)
+				.weightInit(WeightInit.XAVIER)
+				.updater(new Nadam())
+				.activation(Activation.TANH)
+				.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+				.l2(0.0001)
+				.list()
+				.layer(0, new VariationalAutoencoder.Builder()
+						.nIn(4).nOut(2)
+						.activation(Activation.TANH)
+						.pzxActivationFunction(Activation.TANH)
+						.reconstructionDistribution(new GaussianReconstructionDistribution())
+						.lossFunction(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+						.build())
+				.layer(1, new VariationalAutoencoder.Builder()
+						.nIn(2).nOut(2)
+						.activation(Activation.TANH)
+						.pzxActivationFunction(Activation.TANH)
+						.reconstructionDistribution(new GaussianReconstructionDistribution())
+						.lossFunction(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+						.build())
+				.layer(2, new CenterLossOutputLayer.Builder().nIn(2).nOut(4)
+						// Alpha can be thought of as the learning rate for the centers for 
+						// each class
+						.alpha(0.01)
+						// Lambda defines the relative strength of the center loss component.
+				        // lambda = 0.0 is equivalent to training with standard softmax only
+						.lambda(0.0)
+						.weightInit(WeightInit.NORMAL)
+						.activation(Activation.SIGMOID)
+						.lossFunction(LossFunctions.LossFunction.XENT).build())
+				.build();
 		
-		System.out.println(network.summary());
-		
-		for (int i = 0; i < 10000; i++) {
-			network.fit(expected, expected);
-		}
-
-		INDArray actual = network.output(expected);
-		
-		System.out.println(expected);
-		System.out.println("===============================");
-		System.out.println(actual);
+		return new MultiLayerNetwork(conf);
 	}
 	
 	
