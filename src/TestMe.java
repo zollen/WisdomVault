c14c1c8f9c748d0f67318b6161c1cabed1aaa098
Stephen Kong <zollen.kong@gmail.com>
2019-06-24 17:12:26 -0400

    add single variable polynomial approximation

--- src/TestMe.java
+++ src/TestMe.java
@@ -1,53 +1,9 @@
-import java.util.Random;
-
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.dataset.DataSet;
-import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
-import org.nd4j.linalg.dataset.api.preprocessor.NormalizerMinMaxScaler;
-import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
-import org.nd4j.linalg.factory.Nd4j;
-
 public class TestMe {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
-		INDArray arr1 = Nd4j.zeros(new int[] { 1, 10 });
-		INDArray arr2 = Nd4j.zeros(new int[] { 1, 10 });
-		INDArray labels1 = Nd4j.zeros(new int[] { 1, 10 });
-		INDArray labels2 = Nd4j.ones(new int[] { 1, 10 });
-		Random rand = new Random(0);
 		
-		for (int i = 0; i < 10; i++) {
-			arr1.putScalar(new int[] { 0, i }, rand.nextInt(10));
-		}
-		for (int i = 0; i < 10; i++) {
-			arr2.putScalar(new int[] { 0, i }, rand.nextInt(10));
-		}
-		for (int i = 0; i < 10; i++) {
-			labels1.putScalar(new int[] { 0, i }, (i < 6 ? 1 : 0));
-		}
-		
-		
-/*	
-		DataNormalization processor = new CombinedPreProcessor.Builder()
-								.addPreProcessor(new NormalizerStandardize())
-								.addPreProcessor(new NormalizerMinMaxScaler())
-								.build();
-*/		
-		DataNormalization processor1 = new NormalizerStandardize();
-		DataNormalization processor2 = new NormalizerMinMaxScaler(0, 100);
-		
-		DataSet data1 = new DataSet(arr1, labels1);
-		DataSet data2 = new DataSet(arr2, labels2);
-		
-		System.out.println(data2);
-		
-		processor2.fit(data1);
-		processor2.transform(data2);
-		
-		
-		System.out.println(data2);
 		
 	}
 }
---
+++ src/optimization/LagrangePolynomial.java
@@ -0,0 +1,62 @@
+package optimization;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunctionLagrangeForm;
+
+public class LagrangePolynomial {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("p(x) = 1.5x - 4");
+		// p(x) = 1.5x - 4
+		// p(0) = -4.0
+		// p(3.0) = 0.5
+		lagrangeForm(new double[] { 0.0, 3.0 }, new double[] { -4.0, 0.5 }, 2.0, 4.5, 6.0);
+		
+		System.out.println();
+	
+		System.out.println("p(x) = 2x^2 + 5x - 3");
+		
+		// p(x) = 2x^2 + 5x - 3 = (2x - 1)(x + 3)
+		// p(0.0) = -3.0
+		// p(-1.0) = -6.0
+		// p(0.5) = 0.0
+		lagrangeForm(new double[] { 0.0, -1.0, 0.5 }, new double[] { -3.0, -6.0, 0.0 }, 1.0, 2.5, -2.0);
+		
+		System.out.println();
+		
+		// p(x) = 6x^4 + 3x^2 + 8x + 5
+		double [] x = { 0.0, -1.0, -1.5, 2.0, -3.5 };
+		double [] y = new double[5];
+	
+		for (int i = 0; i < 5; i++) {
+			y[i] = func1(x[i]);
+		}
+		
+		lagrangeForm(x, y, 1.0, 3.0, 5.0);
+		
+	}
+	
+	private static double func1(double x) {
+		return 6 * Math.pow(x, 4) + 3 * Math.pow(x, 2) + 8 * x + 5;
+	}
+	
+	public static void lagrangeForm(double [] x, double [] y, double ... tests) {
+		
+		PolynomialFunctionLagrangeForm p = new PolynomialFunctionLagrangeForm(x, y);
+
+		for (int i = 0; i < tests.length; i++)
+			System.out.println("p(" + tests[i] + ") = " + p.value(tests[i]));
+
+
+		System.out.println(p.degree());
+
+		double [] c = p.getCoefficients();
+		System.out.println("coeffs: " + Arrays.stream(c).mapToObj(Double::toString).collect(Collectors.joining(", ")));
+	
+	}
+
+}
