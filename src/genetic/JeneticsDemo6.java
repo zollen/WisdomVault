b9fff9e9b97487a0b006482791310f1bf25df24a
Stephen Kong <zollen.kong@gmail.com>
2019-07-02 16:53:19 -0400

    fix bugs

--- src/genetic/JeneticsDemo6.java
+++ src/genetic/JeneticsDemo6.java
@@ -2,10 +2,19 @@ package genetic;
 
 
 import io.jenetics.DoubleGene;
+import io.jenetics.MeanAlterer;
+import io.jenetics.Mutator;
+import io.jenetics.Optimize;
+import io.jenetics.Phenotype;
+import io.jenetics.StochasticUniversalSelector;
 import io.jenetics.engine.Codec;
 import io.jenetics.engine.Codecs;
+import io.jenetics.engine.Engine;
+import io.jenetics.engine.EvolutionResult;
+import io.jenetics.engine.EvolutionStatistics;
+import io.jenetics.engine.Limits;
+import io.jenetics.stat.DoubleMomentStatistics;
 import io.jenetics.util.DoubleRange;
-import io.jenetics.util.ISeq;
 
 /**
  *  https://github.com/jenetics/jenetics/blob/master/jenetics.prog/README.adoc#examples
@@ -16,26 +25,17 @@ public class JeneticsDemo6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		final Codec<Double, DoubleGene> PARAM = Codecs.ofScalar(DoubleRange.of(1, Math.PI));
+		final Codec<double[], DoubleGene> CODEC = Codecs
+		        .ofVector(DoubleRange.of(0, 1), 3);
+
 		
-		final Codec<Double, DoubleGene> CODEC = Codec.of(
-				ISeq.of(PARAM, PARAM, PARAM),
-				params -> {
-					final double param1 = (double) params[0];
-					final double param2 = (double) params[1];
-					final double param3 = (double) params[2];
-					
-					return 0.0;
-				}
-		);		
-		
-/*		
+
 		final Engine<DoubleGene, Double> engine = Engine
 				.builder(JeneticsDemo6::func, CODEC)
-					.populationSize(500)
+					.populationSize(800)
 					.optimize(Optimize.MAXIMUM)
 					.offspringSelector(new StochasticUniversalSelector<>())
-					.alterers(new Mutator<>(0.03), new MeanAlterer<>(0.5))
+					.alterers(new Mutator<>(0.01), new MeanAlterer<>(0.5))
 					.build();
 
 		final EvolutionStatistics<Double, DoubleMomentStatistics> statistics =
@@ -52,14 +52,14 @@ public class JeneticsDemo6 {
 		
 		System.out.println("Best GenoType: " + result);
 		System.out.println(statistics);
-*/
+
 	}
 	
 
-	public static double func(double a, double b, double c) {
+	public static double func(double [] params) {
 		// f(a, b, c) = 3 * cos(a)^4 + 4 * cos(b)^3 + 2 sin(c)^2 * cos(c)^2 + 5	
-		return 3 * Math.pow(Math.cos(a), 4) + 4 * Math.pow(Math.cos(b), 3) +
-				2 * Math.pow(Math.sin(c), 2) * Math.pow(Math.cos(c), 2) + 5;
+		return 3 * Math.pow(Math.cos(params[0]), 4) + 4 * Math.pow(Math.cos(params[1]), 3) +
+				2 * Math.pow(Math.sin(params[2]), 2) * Math.pow(Math.cos(params[2]), 2) + 5;
 	}
 
 }
--- src/optimization/Lagrangian.java
+++ src/optimization/Lagrangian.java
@@ -17,12 +17,13 @@ public class Lagrangian {
 	 *	f(a, b) = a^2 + a * b + 3
 	 *	c(x) = x^2 + b^2 <= 2
      *
-	 *	L(a, b, l) = a^2 + a * b + 3 + l * (a^2 + b^2 -2)
+	 *	L(a, b, l) = a^2 + a * b + 3 + l * (a^2 + b^2 - 2) 
+	 *        *OR* = a^2 + a * b + 3 
      *
 	 *	With Constraints of inequalities <= or >=
 	 *  -----------------------------------------
 	 *	We must perform another set of optimizations; systematically swap out each inequality 
-	 *  constraint and see if the result is highest/lowest and still satisfies all constraints. 
+	 *  constraint and see if the result is higher/lower and still satisfies all constraints. 
 	 */
 
 	public static void main(String[] args) {
@@ -33,40 +34,46 @@ public class Lagrangian {
 		final double LEARNING_RATE = 0.01;
 		final double THRESHOLD = 0.0001;
 		
+		double lambda1 = 0.1;
+		double lambda2 = 0.1;
 		double a = 1;
 		double b = 1;
 		double c = 1;
-		double lambda = 0.5;
 		
-		for (int iter = 1; iter <= 100000; iter++) {
+		for (int iter = 1; iter <= 10000; iter++) {
 			
 			DerivativeStructure f = formula(a, b, c);
 			
-	//		f = constraint1(f, lambda, a, b, c);  // a + b + c = 3.0
-	//		f = constraint2(f, lambda, a, b, c);  // a^2 + b^2 <= 3
+			f = constraint1(f, lambda1, a, b, c);  // a + b + c = 1
+			f = constraint2(f, lambda2, c);        // c <= 0.8
 			
-			double gradA = f.getPartialDerivative(1, 0, 0, 0);
-			double gradB = f.getPartialDerivative(0, 1, 0, 0);
-			double gradC = f.getPartialDerivative(0, 0, 1, 0);
-			double gradL = f.getPartialDerivative(0, 0, 0, 1);
+			double gradA = f.getPartialDerivative(1, 0, 0, 0, 0);
+			double gradB = f.getPartialDerivative(0, 1, 0, 0, 0);
+			double gradC = f.getPartialDerivative(0, 0, 1, 0, 0);
+			double gradL1 = f.getPartialDerivative(0, 0, 0, 1, 0);
+			double gradL2 = f.getPartialDerivative(0, 0, 0, 0, 1);
 		
 			System.out.println("Iteration: " + iter + 
-					" L(" + ff.format(a) + ", " +
-					ff.format(b) + ", " +
-					ff.format(c) + ", " +
-					ff.format(lambda) +
-					"): " + ff.format(f.getValue()) +  
-					" ∂L/∂λ: " + ff.format(gradL) +
-					" ∂L/∂a: " + ff.format(gradA) +
+					" L(λ1:" + ff.format(lambda1) + 
+					", λ2:" + ff.format(lambda2) + 
+					", a:" + ff.format(a) + 
+					", b:" + ff.format(b) + 
+					", c:" + ff.format(c) + 
+					"): [" + ff.format(f.getValue()) +  
+					"]    ∂L/∂a: " + ff.format(gradA) +
 					" ∂L/∂b: " + ff.format(gradB) +
-					" ∂L/∂c: " + ff.format(gradC));
+					" ∂L/∂c: " + ff.format(gradC) +
+					" ∂L/∂λ1: " + ff.format(gradL1) +
+					" ∂L/∂λ2: " + ff.format(gradL2));
 			
-			lambda -= gradL * LEARNING_RATE;
-			a -= gradA * LEARNING_RATE;
-			b -= gradB * LEARNING_RATE;
-			c -= gradC * LEARNING_RATE;
+			a += gradA * LEARNING_RATE;
+			b += gradB * LEARNING_RATE;
+			c += gradC * LEARNING_RATE;
+			lambda1 -= gradL1 * LEARNING_RATE;
+			lambda2 -= gradL2 * LEARNING_RATE;
 			
-			if (Math.abs(gradL) < THRESHOLD &&
+			if (Math.abs(gradL1) < THRESHOLD &&
+					Math.abs(gradL2) < THRESHOLD &&
 					Math.abs(gradA) < THRESHOLD && 
 					Math.abs(gradB) < THRESHOLD && 
 					Math.abs(gradC) < THRESHOLD)
@@ -76,23 +83,28 @@ public class Lagrangian {
 	
 	private static DerivativeStructure constraint1(DerivativeStructure f, double lamda, double a, double b, double c) {
 		// a + b + c = 4.5
-		DerivativeStructure aa = new DerivativeStructure(4, 1, 0, a);
-		DerivativeStructure bb = new DerivativeStructure(4, 1, 1, b);
-		DerivativeStructure cc = new DerivativeStructure(4, 1, 2, c);
-		DerivativeStructure ll = new DerivativeStructure(4, 1, 3, lamda);
+		DerivativeStructure aa = new DerivativeStructure(5, 1, 0, a);
+		DerivativeStructure bb = new DerivativeStructure(5, 1, 1, b);
+		DerivativeStructure cc = new DerivativeStructure(5, 1, 2, c);
+		DerivativeStructure ll = new DerivativeStructure(5, 1, 3, lamda);
 		
-		return f.add(ll.multiply(aa.add(bb).add(cc).subtract(3.0)));
+		return f.add(ll.multiply(aa.add(bb).add(cc).subtract(1.0)));
 	}
 	
-	private static DerivativeStructure constraint2(DerivativeStructure f, double lamda, double a, double b, double c) {
-		return f;
+	private static DerivativeStructure constraint2(DerivativeStructure f, double lamda, double c) {
+		// c <= 0.8
+		
+		DerivativeStructure cc = new DerivativeStructure(5, 1, 2, c);
+		DerivativeStructure ll = new DerivativeStructure(5, 1, 4, lamda);
+		
+		return f.add(ll.multiply(cc.subtract(0.8)));	
 	}
 	
 	private static DerivativeStructure formula(double a, double b, double c) {
 		// f(a, b, c) = 3 * cos(a)^4 + 4 * cos(b)^3 + 2 sin(c)^2 * cos(c)^2 + 5	
-		DerivativeStructure aa = new DerivativeStructure(4, 1, 0, a);
-		DerivativeStructure bb = new DerivativeStructure(4, 1, 1, b);
-		DerivativeStructure cc = new DerivativeStructure(4, 1, 2, c);
+		DerivativeStructure aa = new DerivativeStructure(5, 1, 0, a);
+		DerivativeStructure bb = new DerivativeStructure(5, 1, 1, b);
+		DerivativeStructure cc = new DerivativeStructure(5, 1, 2, c);
 
 		return new DerivativeStructure(
 						3.0, aa.cos().pow(4.0), 
