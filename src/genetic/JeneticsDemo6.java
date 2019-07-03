59a2abd8dd472d501c8d2b1acc97079e52e0d624
Stephen Kong <zollen.kong@gmail.com>
2019-07-03 06:50:30 -0400

    increase epochs

--- src/genetic/JeneticsDemo6.java
+++ src/genetic/JeneticsDemo6.java
@@ -32,7 +32,7 @@ public class JeneticsDemo6 {
 
 		final Engine<DoubleGene, Double> engine = Engine
 				.builder(JeneticsDemo6::func, CODEC)
-					.populationSize(800)
+					.populationSize(8000)
 					.optimize(Optimize.MAXIMUM)
 					.offspringSelector(new StochasticUniversalSelector<>())
 					.alterers(new Mutator<>(0.01), new MeanAlterer<>(0.5))
@@ -45,10 +45,10 @@ public class JeneticsDemo6 {
 		final Phenotype<DoubleGene, Double> result = engine.stream()
 				// Truncate the evolution stream if no better individual could
 				// be found after 5 consecutive generations.
-				.limit(Limits.bySteadyFitness(5))
+				.limit(Limits.bySteadyFitness(500))
 				.peek(statistics)
 				// Terminate the evolution after maximal 100 generations.
-				.limit(500).collect(EvolutionResult.toBestPhenotype());
+				.limit(50000).collect(EvolutionResult.toBestPhenotype());
 		
 		System.out.println("Best GenoType: " + result);
 		System.out.println(statistics);
