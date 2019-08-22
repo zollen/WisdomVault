1c54724284dc4f51334851aa534a9f63a1c0bd69
Stephen Kong <zollen.kong@gmail.com>
2019-08-22 09:36:55 -0400

    formatted output

--- src/machinelearning/HiddenMarkovModel1.java
+++ src/machinelearning/HiddenMarkovModel1.java
@@ -136,7 +136,7 @@ public class HiddenMarkovModel1 {
 	private static String display1(List<Pair<Integer, DMatrixRMaj>> list) {
 		
 		return list.stream().map(p -> 
-			"{" + SEQUENCE[CONVERTER[p.getKey()]] + "} : " + "[" +
+			"{" + SEQUENCE[CONVERTER[p.getKey()]] + "}: " + "[" +
 			ff.format(p.getValue().get(0, 0)) + ", " +
 			ff.format(p.getValue().get(1, 0)) + "]"
 				).collect(Collectors.joining(", "));
