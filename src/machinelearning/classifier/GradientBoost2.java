ef8813fd0737f0065635d81d2678dc265b6378d0
Stephen Kong <zollen.kong@gmail.com>
2019-09-04 19:12:05 -0400

    researching weka randomtree custom calcuation

--- src/machinelearning/classifier/GradientBoost2.java
+++ src/machinelearning/classifier/GradientBoost2.java
@@ -23,6 +23,7 @@ public class GradientBoost2 {
 	
 	private static final DecimalFormat ff = new DecimalFormat("0.00");
 	private static final double THRESHOLD = 0.00001;
+	private static final double DECISION = 0.5;
 	private static final int TOTAL_TREES = 1000;
 	private static final double LEARNING_RATE = 0.1;
 	
@@ -30,13 +31,13 @@ public class GradientBoost2 {
 		// TODO Auto-generated method stub
 		Equation eq = new Equation();
 		eq.process("A = [" +
-			//	     Height, Color, Gender, Weight
-					"   1.6,     0,      0,   88;" +
-					"   1.6,     1,      1,   76;" +
-					"   1.5,     0,      1,   56;" +
-					"   1.8,     2,      0,   73;" +
-					"   1.5,     1,      0,   77;" +
-					"   1.4,     0,      1,   57 " +
+		 //   Likes Popcorn,   Age,  Color,   Like Movies
+					"     1,    12,      0,    1;" +
+					"     1,    87,      1,    1;" +
+					"     0,    44,      0,    0;" +
+					"     1,    19,      2,    0;" +
+					"     0,    32,      1,    1;" +
+					"     0,    14,      0,    1 " +
 					"]");
 		
 		DMatrixRMaj A = eq.lookupDDRM("A");
@@ -44,10 +45,12 @@ public class GradientBoost2 {
 		// Let's get the average weight as our first prediction
 		// The first prediction suggest all predicted weigths are 71.166667
 		DMatrixRMaj W = CommonOps_DDRM.extractColumn(A, 3, null);
-		
+	
 		/*** The averaging step is actually the derivative calcuation  ***/
-		/*** MSE: Σ 1/2 (Prediction - Actual)^2  ***/
-		double avg = CommonOps_DDRM.sumCols(W, null).get(0, 0) / W.numRows;
+		/** There are 4 people who like movies, but 2 people do not.  **/
+		/** Instead of averging it like the regression example, 
+		 * we use toProbabilty to 'average' **/
+		double avg = toProbability(4.0/2.0);
 		
 		DMatrixRMaj PREV = new DMatrixRMaj(W.numRows, 1);
 		CommonOps_DDRM.fill(PREV, 0.0);
@@ -58,6 +61,7 @@ public class GradientBoost2 {
 		CommonOps_DDRM.fill(AVG, avg);
 		CommonOps_DDRM.subtract(W, AVG, R);
 		
+		
 		DMatrixRMaj r = new DMatrixRMaj(R.numRows, 1);
 		CommonOps_DDRM.fill(r, 0.0);
 		
@@ -66,7 +70,7 @@ public class GradientBoost2 {
 		List<RandomTree> trees = new ArrayList<RandomTree>();
 		
 		// Let's start building trees
-		for (int i = 0; i < TOTAL_TREES; i++) {
+		for (int i = 0; i < 2; i++) {
 					
 			RandomTree tree = new RandomTree();	
 			tree.setMaxDepth(1);   // we have a small toy data, this tree depth would be 3 to 6 
@@ -76,7 +80,7 @@ public class GradientBoost2 {
 			tree.buildClassifier(data);
 			
 			/*** The averaging step is actually the derivative calcuation  ***/
-			/*** MSE: Σ 1/2 (Prediction - Actual)^2  ***/
+			// Σ ( residual / Σ (Previous Probabilty * (1 - Previous Probability)) ) 
 			DMatrixRMaj C = toMatrix(tree, data);
 			
 			trees.add(tree);
@@ -102,6 +106,20 @@ public class GradientBoost2 {
 			for (Instance test : tests)
 				System.out.println(test + "   ==>    " + ff.format(classify(trees, avg, test)));
 		}		
+	}
+	
+	private static double toDecision(double prob) {
+		
+		if (prob >= DECISION)
+			return 1.0;
+		else
+			return 0.0;
+	}
+	
+	private static double toProbability(double val) {
+		
+		return Math.pow(Math.E, Math.log(val)) / 
+				(1.0 + Math.pow(Math.E, Math.log(val)));
 	}
 	
 	private static Instances data(double height, int color, int gender, double weight) {
@@ -156,16 +174,16 @@ public class GradientBoost2 {
 				
 				switch(col) {
 				case 0:
-					data.setValue(attrs.get(col), A.get(row, col));
+					data.setValue(attrs.get(col), attrs.get(col).value((int) A.get(row, col)));
 				break;
 				case 1:
-					data.setValue(attrs.get(col), attrs.get(col).value((int) A.get(row, col)));
+					data.setValue(attrs.get(col), A.get(row, col));
 				break;
 				case 2:
 					data.setValue(attrs.get(col), attrs.get(col).value((int) A.get(row, col)));
 				break;
 				default:
-					data.setValue(attrs.get(col), R.get(row, 0));
+					data.setValue(attrs.get(col), attrs.get(col).value((int) A.get(row, col)));
 				}
 			}
 			
@@ -180,20 +198,21 @@ public class GradientBoost2 {
 	
 	private static ArrayList<Attribute> setup() {
 		
+		final ArrayList<String> likesVals = new ArrayList<String>();
+		likesVals.add("No");
+		likesVals.add("Yes");
+		
+		
 		final ArrayList<String> colorVals = new ArrayList<String>();
 		colorVals.add("Blue");
 		colorVals.add("Green");
 		colorVals.add("Red");
 		
-		final ArrayList<String> genderVals = new ArrayList<String>();
-		genderVals.add("Male");
-		genderVals.add("Female");
-		
 		ArrayList<Attribute> attrs = new ArrayList<Attribute>();
-		Attribute attr1 = new Attribute("Height");
-		Attribute attr2 = new Attribute("Color", colorVals);
-		Attribute attr3 = new Attribute("Gender", genderVals);
-		Attribute attr4 = new Attribute("Residual");
+		Attribute attr1 = new Attribute("Like Popcorn", likesVals);
+		Attribute attr2 = new Attribute("Age");
+		Attribute attr3 = new Attribute("Color", colorVals);
+		Attribute attr4 = new Attribute("Like Movies", likesVals);
 		attrs.add(attr1);
 		attrs.add(attr2);
 		attrs.add(attr3);
